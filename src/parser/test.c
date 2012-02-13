a4b8454184ab36a3a9c3d1d82e9396392ad948f9
Aaron Leung <aaron.leung@moovweb.com>
2012-02-13 14:26:47 -0800

    Refactoring and fixing stuff.

--- src/parser/test.c
+++ src/parser/test.c
@@ -2,11 +2,11 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-#include "text_prefix_matchers.h"
+#include "prefix_primitives.h"
 
 int main() {
   char *s = "'this is a \"string\" now' blah blah blah";
-  int l = text_has_delimited_prefix(s, "'", "'", 1);
+  int l = prefix_is_string_constant(s);
   if (l) {
     printf("matched a string literal of length %d:\n", l);
     int i;
@@ -15,10 +15,15 @@ int main() {
     }
     putchar('\n');
   }
+  else {
+    printf("matched %d characters\n", l);
+  }
   
   unsigned char x;
   printf("By the way, punctuation symbols are:\n");
   for (x = '\0'; x < 128; x++) if (ispunct(x)) printf("%c", x);
   putchar('\n');
+  printf("By the way, 0 || 24 is: %d\n", 0 || 24);
+  printf("And 24 || 0 is: %d\n", 24 || 0);
   return 0;
 }
