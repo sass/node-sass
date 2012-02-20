1f73d1625cfdb0f17167ca6c4676b303b57e3399
Aaron Leung <aaron.leung@moovweb.com>
2012-02-20 11:29:36 -0800

    Testing and debugging the context initialization functions.

--- src/context.h
+++ src/context.h
@@ -4,8 +4,10 @@ typedef struct {
   char *path;  /* the full directory+filename of the source file */
   char *src;   /* the text of the entire source file */
   char *pos;   /* keeps track of the parser/scanner's current position */
+  size_t line; /* the number of the line currently being parsed/scanned */
   jmp_buf env; /* the top of the exception-handling stack */
   /* more to come */
 } sass_context;
 
-sass_context *make_sass_context_from_file(char *path);
+sass_context *sass_make_context_from_file(char *path);
+sass_context *sass_make_context_from_string(char *src);
