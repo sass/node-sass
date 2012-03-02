4390954e03269fe598906787ec8905c5aa72e5fc
Aaron Leung <aaron.leung@moovweb.com>
2012-03-01 17:04:40 -0800

    Adding crappy command-line options so that I can check multiple output styles without recompiling. Make sure you get the command-line options right or there will be segfaults.

--- sassc.cpp
+++ sassc.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include "document.hpp"
 
 using namespace Sass;
@@ -7,16 +8,27 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
-    cout << "Hey, I need a file to read!" << endl;
+    cout << "Hey, I at least need a file to read!" << endl;
     return 0;
   }
-
-  Document doc(argv[1], 0);
+  
+  char* path;
+  Document::CSS_Style style = Document::nested;
+  
+  for (int i = 1; i < argc; ++i) {
+    if (string(argv[i]) == "--style") {
+      string style_name(argv[++i]);
+      if (style_name == "nested") style = Document::nested;
+      else if (style_name == "expanded") style = Document::expanded;
+    }
+    else {
+      path = argv[i];
+    }
+  }
+    
+  Document doc(path, 0);
   doc.parse_scss();
-  // string output = doc.emit_css(doc.expanded);
-  // cout << output;
-  // cout << endl;
-  string output = doc.emit_css(doc.nested);
+  string output = doc.emit_css(style);
   cout << output;
 
   return 0;
