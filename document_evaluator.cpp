aa3b799bbbd8d8d5cad1e016a6c0a31a324d7df2
Aaron Leung <aaron.leung@moovweb.com>
2012-03-27 12:19:34 -0700

    Bam, all tests are passing again.

--- document_evaluator.cpp
+++ document_evaluator.cpp
@@ -9,12 +9,31 @@ namespace Sass {
       Node n(context.pending[i]);
       switch (n.type)
       {
-        case Node::assignment:
-          context.environment[n[0].token] = eval(n[1], context.environment);
-          break;
-        case Node::rule:
-          n[1] = eval(n[1], context.environment);
-          break;
+        case Node::assignment: {
+          Node val(n[1]);
+          if (val.type == Node::comma_list || val.type == Node::space_list) {
+            for (int i = 0; i < val.size(); ++i) {
+              if (val[i].eval_me) val[i] = eval(val[i], context.environment);
+            }
+          }
+          else {
+            val = eval(val, context.environment);
+          }
+          context.environment[n[0].token] = val;
+        } break;
+
+        case Node::rule: {
+          // treat top-level lists differently from nested ones
+          Node rhs(n[1]);
+          if (rhs.type == Node::comma_list || rhs.type == Node::space_list) {
+            for (int i = 0; i < rhs.size(); ++i) {
+              if (rhs[i].eval_me) rhs[i] = eval(rhs[i], context.environment);
+            }
+          }
+          else {
+            n[1] = eval(n[1], context.environment);
+          }
+        } break;
       }
     }
   }
