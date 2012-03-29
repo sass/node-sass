dc2eeef094cdaff83437c97d58ffed3e1e63de6f
Aaron Leung <aaron.leung@moovweb.com>
2012-03-28 17:27:24 -0700

    Removing some commented code.

--- document_eval_pending.cpp
+++ document_eval_pending.cpp
@@ -42,48 +42,8 @@ namespace Sass {
           Token name(n[0].token);
           Node args(n[1]);
           Node mixin(context.global_env[name]);
-          // Node params(mixin[1]);
-          // Node body(mixin[2].clone());
-
           n.children->pop_back();
           n.children->pop_back();
-
-          // Environment m_env;
-          //  // bind arguments
-          //  for (int i = 0, j = 0; i < args.size(); ++i) {
-          //    if (args[i].type == Node::assignment) {
-          //      Node arg(args[i]);
-          //      Token name(arg[0].token);
-          //      if (!m_env.query(name)) {
-          //        m_env[name] = eval(arg[1], context.global_env);
-          //      }
-          //    }
-          //    else {
-          //      // TO DO: ensure (j < params.size())
-          //      Node param(params[j]);
-          //      Token name(param.type == Node::variable ? param.token : param[0].token);
-          //      m_env[name] = eval(args[i], context.global_env);
-          //      ++j;
-          //    }
-          //  }
-          //  // plug the holes with default arguments if any
-          //  for (int i = 0; i < params.size(); ++i) {
-          //    if (params[i].type == Node::assignment) {
-          //      Node param(params[i]);
-          //      Token name(param[0].token);
-          //      if (!m_env.query(name)) {
-          //        m_env[name] = eval(param[1], context.global_env);
-          //      }
-          //    }
-          //  }
-          //  m_env.link(context.global_env);
-          //  
-          //  for (int i = 0; i < body.size(); ++i) {
-          //    body[i] = eval(body[i], m_env);
-          //  }
-          //  
-          //  n += body;
-          
           n += apply(mixin, args, context.global_env);
         } break;
       }
