82b95b9e885a33629b060af05a67a832d2051d6a
Aaron Leung <aaron.leung@moovweb.com>
2012-04-18 16:34:55 -0700

    Fixing the memory-leak test. No leaks.

--- loop.c
+++ loop.c
@@ -14,28 +14,31 @@ int main(int argc, char** argv)
 	
   while (1) {
     getrusage(who, &r);
-    printf("Memory usage: %ld", r.ru_maxrss);
+    printf("Memory usage: %ld\n", r.ru_maxrss);
     struct sass_file_context* ctx = sass_new_file_context();
     ctx->options.include_paths = "::/blah/bloo/fuzz:/slub/flub/chub::/Users/Aaron/dev/libsass/::::/huzz/buzz:::";
     ctx->options.output_style = SASS_STYLE_NESTED;
     ctx->input_path = argv[1];
 
-    sass_file_compile(ctx);
+    sass_compile_file(ctx);
 
     if (ctx->error_status) {
       if (ctx->error_message) printf("%s", ctx->error_message);
       else printf("An error occured; no error message available.\n");
+          sass_free_file_context(ctx);
       break;
     }
     else if (ctx->output_string) {
+      sass_free_file_context(ctx);
       continue;
     }
     else {
       printf("Unknown internal error.\n");
+          sass_free_file_context(ctx);
       break;
     }
 
-    sass_file_free_context(ctx);
+
   }
 	return 0;
 }
