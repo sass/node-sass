e6e3918eac114c70ada0a85efe8700d4fc43917d
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-04-21 11:48:01 -0700

    fixing up extconf

--- src/extconf.rb
+++ src/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
 # .. more stuff
-$LIBPATH.push(Config::CONFIG['libdir'])
+#$LIBPATH.push(Config::CONFIG['libdir'])
+$CFLAGS << " #{ENV["CFLAGS"]}"
+$LIBS << " #{ENV["LIBS"]}"
 create_makefile("libsass")
