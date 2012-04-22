364ed8664503f963a88a47e07b60c3b0550c7017
Aaron Leung <aaron.leung@moovweb.com>
2012-04-21 19:52:14 -0700

    Removing unneeded comments and other test stuff.

--- sassc.c
+++ sassc.c
@@ -1,22 +1,3 @@
-// 
-//   This is when you want to compile a whole folder of stuff
-// 
-// var opts = sass_new_context();
-// opts->sassPath = "/Users/hcatlin/dev/asset/stylesheet";
-// opts->cssPath = "/Users/hcatlin/dev/asset/stylesheets/.css";
-// opts->includePaths = "/Users/hcatlin/dev/asset/stylesheets:/Users/hcatlin/sasslib";
-// opts->outputStyle => SASS_STYLE_COMPRESSED;
-// sass_compile(opts, &callbackfunction);
-// 
-// 
-//   This is when you want to compile a string
-// 
-// opts = sass_new_context();
-// opts->inputString = "a { width: 50px; }";
-// opts->includePaths = "/Users/hcatlin/dev/asset/stylesheets:/Users/hcatlin/sasslib";
-// opts->outputStyle => SASS_STYLE_EXPANDED;
-// var cssResult = sass_compile(opts, &callbackfunction);
-
 #include <stdio.h>
 #include "src/sass_interface.h"
 
@@ -28,7 +9,7 @@ int main(int argc, char** argv)
 	}
 	
 	struct sass_file_context* ctx = sass_new_file_context();
-	ctx->options.include_paths = "::/blah/bloo/fuzz:/slub/flub/chub::/Users/Aaron/dev/libsass/::::/huzz/buzz:::";
+	ctx->options.include_paths = "";
 	ctx->options.output_style = SASS_STYLE_NESTED;
 	ctx->input_path = argv[1];
 		
