51d3d500d1c7e0a5aa13333f2f68f2ffa12c0498
Andrew Nesbitt <andrewnez@gmail.com>
2012-04-24 21:50:08 +0100

    Getting closer

--- sass.cpp
+++ sass.cpp
@@ -1,14 +1,20 @@
 #include <v8.h>
 #include <node.h>
+#include "libsass/sass_interface.h"
 
 using namespace v8;
 
 Handle<Value> Render(const Arguments& args) {
     HandleScope scope;
 
-    Local<String> result = args[0]->ToString();
+    struct sass_context* ctx = sass_new_context();
 
-    return scope.Close(result);
+    ctx->input_string = args[0]->ToString();
+    ctx->options.output_style = SASS_STYLE_NESTED;
+
+    sass_compile(ctx);
+
+    return scope.Close(String::New(ctx->output_string));
 }
 
 void RegisterModule(v8::Handle<v8::Object> target) {
