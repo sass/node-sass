4808d3220d33fce421cf6511c58e792d7651db59
Aaron Leung <aaron.leung@moovweb.com>
2012-05-01 18:19:45 -0700

    Unit testing the new node stuff. This file is gonna' go away.

--- test_node_factory.cpp
+++ test_node_factory.cpp
@@ -12,7 +12,24 @@ int main()
   using namespace std;
   
   cout << sizeof(Node) << endl;
-  cout << sizeof(Node_Impl) << endl;
+  cout << sizeof(Node_Impl) << endl << endl;
+  
+  Node_Factory make = Node_Factory();
+  
+  Node interior(make.node(block, 0, 0, 3));
+  
+  cout << interior.size() << endl;
+  cout << interior.has_children() << endl;
+  cout << interior.eval_me() << endl << endl;
+  
+  Node num(make.node(0, 0, 255, 123, 32));
+  
+  cout << num.size() << endl;
+  cout << num.has_children() << endl;
+  cout << num.has_statements() << endl << endl;
+  
+  cout << num[1].is_numeric() << endl;
+  cout << num[1].numeric_value() << endl << endl;
   
   return 0;
 }
