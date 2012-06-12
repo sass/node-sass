0f7fba37622e8328bd1f467e7458ce7c4ab78207
Dean Mao <deanmao@gmail.com>
2012-06-12 08:46:15 -0700

    add precompiled binaries

--- sass.js
+++ sass.js
@@ -1,4 +1,17 @@
-var binding = require('./build/Release/binding')
+var binding;
+var fs = require('fs');
+try {
+  if (fs.realpathSync(__dirname + '/build')) {
+    // use the build version if it exists
+    binding = require(__dirname + '/build/Release/binding');
+  }
+} catch (e) {
+  // default to a precompiled binary if no build exists
+  binding = require(__dirname + '/precompiled/'+process.platform+'/binding');
+}
+if (binding === null) {
+  throw new Error('Cannot find appropriate binary library for node-sass');
+}
 
 exports.render = binding.render
 exports.middleware = require('./lib/middleware');
