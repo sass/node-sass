ec641613bd7f4cb3c810941911fc616399463303
Aaron Leung <aaron.leung@moovweb.com>
2012-08-31 12:08:01 -0700

    Moving the environment definition into its own file so I can eliminate a circular dependency.

---
+++ environment.hpp
@@ -0,0 +1,41 @@
+#define SASS_ENVIRONMENT
+
+#include <map>
+
+#ifndef SASS_NODE
+#include "node.hpp"
+#endif
+
+namespace Sass {
+  using std::map;
+
+  struct Environment {
+    map<Token, Node> current_frame;
+    Environment* parent;
+    Environment* global;
+    
+    Environment()
+    : current_frame(map<Token, Node>()), parent(0), global(0)
+    { }
+    
+    void link(Environment& env)
+    {
+      parent = &env;
+      global = parent->global ? parent->global : parent;
+    }
+    
+    bool query(const Token& key) const
+    {
+      if (current_frame.count(key)) return true;
+      else if (parent)              return parent->query(key);
+      else                          return false;
+    }
+    
+    Node& operator[](const Token& key)
+    {
+      if (current_frame.count(key)) return current_frame[key];
+      else if (parent)              return (*parent)[key];
+      else                          return current_frame[key];
+    }
+  };
+}
