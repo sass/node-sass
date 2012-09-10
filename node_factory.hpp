55d82671d71b93131c502347b6dcffaf49cb89ec
Aaron Leung <aaron.leung@moovweb.com>
2012-09-09 19:29:05 -0700

    Cloning values fetched from variables in order to set their paths and line-numbers to the point of reference. (Using the original values would result in paths and line-numbers being reported as the point of assignment, which is not helpful for debugging.)

--- node_factory.hpp
+++ node_factory.hpp
@@ -17,9 +17,11 @@ namespace Sass {
     Node_Impl* alloc_Node_Impl(Node::Type type, string file, size_t line);
     // returns a deep-copy of its argument
     Node_Impl* alloc_Node_Impl(Node_Impl* ip);
+    Node_Impl* alloc_Node_Impl(string& path, size_t line, Node_Impl* ip);
   public:
     // for cloning nodes
     Node operator()(const Node& n1);
+    Node operator()(string& path, size_t line, const Node& n1);
     // for making leaf nodes out of terminals/tokens
     Node operator()(Node::Type type, string file, size_t line, Token t);
     // for making boolean values or interior nodes that have children
