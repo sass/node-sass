ff5162bbacd3e12d07fd903a0987c7daa66def25
Aaron Leung <aaron.leung@moovweb.com>
2013-05-06 11:40:12 -0700

    Reporting an error on all BOMs except UTF-8.

--- document_parser.cpp
+++ document_parser.cpp
@@ -1487,19 +1487,26 @@ namespace Sass {
   void Document::read_bom()
   {
     size_t skip = 0;
+    string encoding;
+    bool utf_8 = false;
     switch ((unsigned char) source[0]) {
     case 0xEF:
       skip = check_bom_chars(source, utf_8_bom, 3);
+      encoding = "UTF-8";
+      utf_8 = true;
       break;
     case 0xFE:
       skip = check_bom_chars(source, utf_16_bom_be, 2);
+      encoding = "UTF-16 (big endian)";
       break;
     case 0xFF:
       skip = check_bom_chars(source, utf_16_bom_le, 2);
       skip += (skip ? check_bom_chars(source, utf_32_bom_le, 4) : 0);
+      encoding = (skip == 2 ? "UTF-16 (little endian)" : "UTF-32 (little endian)");
       break;
     case 0x00:
       skip = check_bom_chars(source, utf_32_bom_be, 4);
+      encoding = "UTF-32 (big endian)";
       break;
     case 0x2B:
       skip = check_bom_chars(source, utf_7_bom_1, 4)
@@ -1507,23 +1514,30 @@ namespace Sass {
            | check_bom_chars(source, utf_7_bom_3, 4)
            | check_bom_chars(source, utf_7_bom_4, 4)
            | check_bom_chars(source, utf_7_bom_5, 5);
+      encoding = "UTF-7";
       break;
     case 0xF7:
       skip = check_bom_chars(source, utf_1_bom, 3);
+      encoding = "UTF-1";
       break;
     case 0xDD:
       skip = check_bom_chars(source, utf_ebcdic_bom, 4);
+      encoding = "UTF-EBCDIC";
       break;
     case 0x0E:
       skip = check_bom_chars(source, scsu_bom, 3);
+      encoding = "SCSU";
       break;
     case 0xFB:
       skip = check_bom_chars(source, bocu_1_bom, 3);
+      encoding = "BOCU-1";
       break;
     case 0x84:
       skip = check_bom_chars(source, gb_18030_bom, 4);
+      encoding = "GB-18030";
       break;
     }
+    if (skip > 0 && !utf_8) throw_syntax_error("only UTF-8 documents are currently supported; your document appears to be " + encoding);
     position += skip;
   }
 
