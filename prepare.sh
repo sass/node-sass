38ca2acdeeddec8197b13b559aa1e7ea85dbd3ac
Luis Silva <luis.dasilva@mailonline.co.uk>
2013-10-16 00:26:06 +0100

    luis: update script to not execute when in the main project

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Exists to fully update the git repo that you are sitting in...
-#sudo git pull && git submodule init && git submodule update && git submodule status
-#sudo git submodule foreach
 set -e
 
-git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-    while read path_key path
-    do
-        url_key=$(echo $path_key | sed 's/\.path/.url/')
-        url=$(git config -f .gitmodules --get "$url_key")
-        git clone $url $path
-    done
+if ! [ $(PWD) -ef $(git rev-parse --show-toplevel) ];
+    then
+        git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+        while read path_key path
+        do
+            url_key=$(echo $path_key | sed 's/\.path/.url/')
+            url=$(git config -f .gitmodules --get "$url_key")
+            git clone $url $path
+        done
+fi
