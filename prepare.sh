a00721e9d22979c43a962a8f04e1c997769d483d
Luis Silva <luis.dasilva@mailonline.co.uk>
2013-10-15 21:55:59 +0100

    test with new version of git

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Exists to fully update the git repo that you are sitting in...
-cd "$(sudo git rev-parse --show-toplevel)"
 sudo git pull && git submodule init && git submodule update && git submodule status
