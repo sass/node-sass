be740fcd7c4362e7a14eaeb9b7459fd5f29a7827
Luis Silva <luis.dasilva@mailonline.co.uk>
2013-10-16 01:09:34 +0100

    update script

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Exists to fully update the git repo that you are sitting in...
+#clone git submodules because the submodules aren't a npm project
 set -e
 
-if ! [ $(PWD) -ef $(git rev-parse --show-toplevel) ];
-    then
-        git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-        while read path_key path
-        do
-            url_key=$(echo $path_key | sed 's/\.path/.url/')
-            url=$(git config -f .gitmodules --get "$url_key")
-            git clone $url $path
-        done
-fi
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+while read path_key path
+do
+    url_key=$(echo $path_key | sed 's/\.path/.url/')
+    url=$(git config -f .gitmodules --get "$url_key")
+    rm -rf $path
+    git clone $url $path
+done
