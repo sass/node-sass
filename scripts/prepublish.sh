1ffb22d0cca196bde2f173bc3018cb2f3fc98c3a
Andrew Nesbitt <andrewnez@gmail.com>
2013-10-19 07:15:37 -0700

    Fixed cp argument order in prepublish script

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -6,7 +6,7 @@ if [ "$CI" == "true" ]; then
 	echo "Skipping prepublish on CI builds";
 elif [ -d "$DIRECTORY" ]; then
 	echo "Copying binaries to bin"
-	cp $DIRECTORY/*-v8-* bin/ -R
+	cp -R $DIRECTORY/*-v8-* bin/
 else
 	echo "Skipping bin copy. Please clone https://github.com/andrew/node-sass-binaries for prebuilt binaries";
 fi
