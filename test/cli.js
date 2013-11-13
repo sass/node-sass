99411cf608b91fb5ff5fdafc69ab74633f7c2f8c
hongymagic <d.hong@me.com>
2013-11-13 22:36:24 +1100

    Update libsass to cc595a8

--- test/cli.js
+++ test/cli.js
@@ -68,7 +68,7 @@ describe('cli', function() {
     ]);
     emitter.on('error', done);
     emitter.on('write', function(err, file, css){
-      assert.equal(css.trim(), 'body {\n  background: red;\n  color: blue; }');
+      assert.equal(css.trim(), 'body {\n  background: red;\n  color: #0000FF; }');
       fs.unlink(file, done);
     });
   });
