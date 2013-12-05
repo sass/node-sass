ece02b699a14a4d30a849de42c955328b798295a
Aaron Leung <aaron.leung@moovweb.com>
2013-12-05 14:08:23 -0800

    making the tester easier to use

--- superselector_test.cpp
+++ superselector_test.cpp
@@ -9,34 +9,29 @@ using namespace Sass;
 Context ctx = Context(Context::Data());
 To_String to_string;
 
-Compound_Selector* compound_selector(string src, Context& ctx)
+Compound_Selector* compound_selector(string src)
 { return Parser::from_c_str(src.c_str(), ctx, "", 0).parse_simple_selector_sequence(); }
 
-void check(Compound_Selector* s1, Compound_Selector* s2)
+void check(string s1, string s2)
 {
   cout << "Is "
-       << s1->perform(&to_string)
+       << s1
        << " a superselector of "
-       << s2->perform(&to_string)
+       << s2
        << "?\t"
-       << s1->is_superselector_of(s2)
+       << compound_selector(s1 + ";")->is_superselector_of(compound_selector(s2 + ";"))
        << endl;
 }
 
 int main() {
-  Compound_Selector* s1 = compound_selector(".foo.bar;", ctx);
-  Compound_Selector* s2 = compound_selector(".foo;", ctx);
-  Compound_Selector* s3 = compound_selector("div.foo;", ctx);
-  Compound_Selector* s4 = compound_selector("div.bar.foo;", ctx);
-  Compound_Selector* s5 = compound_selector("p.foo;", ctx);
 
-  check(s2, s1);
-  check(s1, s2);
-  check(s1, s3);
-  check(s2, s3);
-  check(s3, s2);
-  check(s3, s4);
-  check(s5, s4);
+  check(".foo", ".foo.bar");
+  check(".foo.bar", ".foo");
+  check(".foo.bar", "div.foo");
+  check(".foo", "div.foo");
+  check("div.foo", ".foo");
+  check("div.foo", "div.bar.foo");
+  check("p.foo", "div.bar.foo");
 
   return 0;
 }
