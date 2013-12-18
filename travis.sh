77e29e48257b9124c8947a5a44aa4aa8bf52e720
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-18 17:51:54 -0500

    Fix travis.sh script

--- travis.sh
+++ travis.sh
@@ -5,10 +5,11 @@ set -e
 if [ "x$AUTOTOOLS" == "xyes" ]; then
 	autoreconf
 	./configure --enable-tests \
-	            --with-sassc-dir=$SASS_SASSC_PATH \
-	            --with-sass-spec-dir=$SASS_SPEC_PATH
-	make
+		    --with-sassc-dir=$SASS_SASSC_PATH \
+		    --with-sass-spec-dir=$SASS_SPEC_PATH \
+		    --prefix=$TRAVIS_BUILD_DIR
+	make install
+	cp $TRAVIS_BUILD_DIR/lib/libsass.a $TRAVIS_BUILD_DIR
 fi
 
 make LOG_FLAGS=--skip VERBOSE=1 test_build
-
