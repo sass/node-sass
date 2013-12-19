d439c0be1f058c6102f8f231d4df591a09f3920c
Joshua Bussdieker <jbussdieker@gmail.com>
2013-12-18 23:18:57 -0500

    Use autoreconf -i and add gitkeep to m4 to make automake happy

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "x$AUTOTOOLS" == "xyes" ]; then
-	./autogen.sh
+	autoreconf -i
 	./configure --enable-tests \
 		    --with-sassc-dir=$SASS_SASSC_PATH \
 		    --with-sass-spec-dir=$SASS_SPEC_PATH \
