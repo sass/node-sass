c9d9294c1b25f0ffd0a3340f304d9399591e06eb
Nick Schonning <nschonni@gmail.com>
2014-01-02 22:57:51 -0500

    JSHint Middleware example

--- examples/middleware.js
+++ examples/middleware.js
@@ -3,18 +3,18 @@
  * Module dependencies.
  */
 
-var connect = require('connect')
-  , sass = require('../sass');
+var connect = require('connect'),
+  sass = require('../sass');
 
 // Setup server
 // $ curl http://localhost:3000/test.css
 
 var server = connect.createServer(
   sass.middleware({
-      src: __dirname
-    , dest: __dirname + '/public'
-    , debug: true
-    , outputStyle: 'compressed'
+    src: __dirname,
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'compressed'
   }),
   connect.static(__dirname + '/public')
 );
