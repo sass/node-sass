9e57bdf98ede7d44e51ba0cc8c4fea7cfcbcaa33
Aaron Leung <aaron.leung@moovweb.com>
2014-01-07 12:46:46 -0800

    Commenting out the old @extend code.

--- extend.cpp
+++ extend.cpp
@@ -24,54 +24,54 @@ namespace Sass {
     Selector_List* sg = static_cast<Selector_List*>(r->selector());
     Selector_List* ng = 0;
     bool extended = false;
-    if (sg->has_placeholder()) {
-      // To_String to_string;
-      Compound_Selector* placeholder = new (ctx.mem) Compound_Selector(sg->path(), sg->position(), 1);
-      *placeholder << sg->find_placeholder();
-      // cerr << "placeholder: " << placeholder->perform(&to_string) << endl;
-      // if the placeholder needs to be subbed
-      if (extensions.count(*placeholder)) {
-        // cerr << "need to sub " << placeholder->perform(&to_string) << " " << extensions.count(*placeholder) << " times" << endl;
-        // perform each substitution and append it to the selector group of the ruleset
-        ng = new (ctx.mem) Selector_List(sg->path(), sg->position(), extensions.count(*placeholder));
-        for (multimap<Compound_Selector, Complex_Selector*>::iterator extender = extensions.lower_bound(*placeholder), E = extensions.upper_bound(*placeholder);
-             extender != E;
-             ++extender) {
-          // cerr << "performing a substitution: " << placeholder->perform(&to_string) << " -> " << extender->second->perform(&to_string) << endl;
-          Contextualize sub_plc(ctx, 0, 0, backtrace, placeholder, extender->second);
-          Selector_List* subbed = static_cast<Selector_List*>(sg->perform(&sub_plc));
-          // if (subbed) cerr << "subbed: " << subbed->perform(&to_string) << endl;
-          *ng += subbed;
-          extended = true;
-        }
-        ng->has_placeholder(false);
-      }
-      // otherwise prevent it from rendering
-      else {
-        // r->selector(0);
-      }
-    }
-    else {
-      To_String to_string;
-      ng = new (ctx.mem) Selector_List(sg->path(), sg->position(), sg->length());
-      // for each selector in the group
-      for (size_t i = 0, L = sg->length(); i < L; ++i) {
-        Complex_Selector* sel = (*sg)[i];
-        *ng << sel;
-        // if it's supposed to be extended
-        Compound_Selector* sel_base = sel->base();
-        if (sel_base && extensions.count(*sel_base)) {
-          // extend it wrt each of its extenders
-          for (multimap<Compound_Selector, Complex_Selector*>::iterator extender = extensions.lower_bound(*sel_base), E = extensions.upper_bound(*sel_base);
-               extender != E;
-               ++extender) {
-            *ng += generate_extension(sel, extender->second);
-            extended = true;
-          }
-        }
-      }
-    }
-    if (extended) r->selector(ng);
+    // if (sg->has_placeholder()) {
+    //   // To_String to_string;
+    //   Compound_Selector* placeholder = new (ctx.mem) Compound_Selector(sg->path(), sg->position(), 1);
+    //   *placeholder << sg->find_placeholder();
+    //   // cerr << "placeholder: " << placeholder->perform(&to_string) << endl;
+    //   // if the placeholder needs to be subbed
+    //   if (extensions.count(*placeholder)) {
+    //     // cerr << "need to sub " << placeholder->perform(&to_string) << " " << extensions.count(*placeholder) << " times" << endl;
+    //     // perform each substitution and append it to the selector group of the ruleset
+    //     ng = new (ctx.mem) Selector_List(sg->path(), sg->position(), extensions.count(*placeholder));
+    //     for (multimap<Compound_Selector, Complex_Selector*>::iterator extender = extensions.lower_bound(*placeholder), E = extensions.upper_bound(*placeholder);
+    //          extender != E;
+    //          ++extender) {
+    //       // cerr << "performing a substitution: " << placeholder->perform(&to_string) << " -> " << extender->second->perform(&to_string) << endl;
+    //       Contextualize sub_plc(ctx, 0, 0, backtrace, placeholder, extender->second);
+    //       Selector_List* subbed = static_cast<Selector_List*>(sg->perform(&sub_plc));
+    //       // if (subbed) cerr << "subbed: " << subbed->perform(&to_string) << endl;
+    //       *ng += subbed;
+    //       extended = true;
+    //     }
+    //     ng->has_placeholder(false);
+    //   }
+    //   // otherwise prevent it from rendering
+    //   else {
+    //     // r->selector(0);
+    //   }
+    // }
+    // else {
+    //   To_String to_string;
+    //   ng = new (ctx.mem) Selector_List(sg->path(), sg->position(), sg->length());
+    //   // for each selector in the group
+    //   for (size_t i = 0, L = sg->length(); i < L; ++i) {
+    //     Complex_Selector* sel = (*sg)[i];
+    //     *ng << sel;
+    //     // if it's supposed to be extended
+    //     Compound_Selector* sel_base = sel->base();
+    //     if (sel_base && extensions.count(*sel_base)) {
+    //       // extend it wrt each of its extenders
+    //       for (multimap<Compound_Selector, Complex_Selector*>::iterator extender = extensions.lower_bound(*sel_base), E = extensions.upper_bound(*sel_base);
+    //            extender != E;
+    //            ++extender) {
+    //         *ng += generate_extension(sel, extender->second);
+    //         extended = true;
+    //       }
+    //     }
+    //   }
+    // }
+    // if (extended) r->selector(ng);
 
     // let's try the new stuff here; eventually it should replace the preceding
     set<Compound_Selector> seen;
