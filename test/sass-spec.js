a82921ab7ea1fa66180f334ab87a997e1a1f335c
Nick Schonning <nschonni@gmail.com>
2014-01-12 21:55:12 -0500

    Skip 'todo' and 'benchmark' test suites

--- test/sass-spec.js
+++ test/sass-spec.js
@@ -16,6 +16,10 @@ fs.exists(sassSpecPath, function(exists) {
 
 var suites = fs.readdirSync(path.join(sassSpecPath, 'spec'));
 
+suites = suites.filter(function(element){
+  return !(element === 'todo' || element === 'benchmarks');
+});
+
 // Each subfolder of spec is a separate suite of Sass tests
 suites.forEach(function (suiteDir) {
 
