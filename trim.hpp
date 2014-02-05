da221d89a5900aff3ac1ce146fdc78d81b753651
Aaron Leung <aaron.leung@moovweb.com>
2014-02-05 10:49:00 -0800

    commenting out this unfinished stuff

--- trim.hpp
+++ trim.hpp
@@ -1,8 +1,8 @@
-#define SASS_TRIM
-#include <vector>
+// #define SASS_TRIM
+// #include <vector>
 
-namespace Sass {
-  using std::vector;
-  class Complex_Selector;
-  vector<vector<Complex_Selector*> > trim(vector<vector<Complex_Selector*> >);
-}
+// namespace Sass {
+//   using std::vector;
+//   class Complex_Selector;
+//   vector<vector<Complex_Selector*> > trim(vector<vector<Complex_Selector*> >);
+// }
