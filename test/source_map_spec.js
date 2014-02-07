sass/node-sass

commit 04dd6f226860f5a23c64e0c93aaee90b92ba7cf6

Fixed a strcpy error in binding.cpp

binding.cpp
@@ -70,7 +70,7 @@ void extractOptions(_NAN_METHOD_ARGS, void* cptr, sass_context_wrapper* ctx_w, b
     if (source_comments == SASS_SOURCE_COMMENTS_MAP) {
       String::AsciiValue dstr(args[7]);
       ctx->source_map_file = new char[strlen(*dstr)+1];
-      strcpy(ctx->source_map_file, *dstr);
+      ctx->source_map_file = *dstr;
     }
   } else {
     sass_context *ctx = (sass_context*)cptr;

commit f42abaf1fefabdd8bc33c199e013cf7457d780a6

Fix sourceMappingURL
Broken from 04dd6f2268

Simple fix to cast the const pointer to a non-const pointer. String
seems to still need to be copied.

binding.cpp
@@ -70,7 +70,7 @@ void extractOptions(_NAN_METHOD_ARGS, void* cptr, sass_context_wrapper* ctx_w, b
     if (source_comments == SASS_SOURCE_COMMENTS_MAP) {
       String::AsciiValue dstr(args[7]);
       ctx->source_map_file = new char[strlen(*dstr)+1];
-      ctx->source_map_file = *dstr;
+      strcpy((char*) ctx->source_map_file, *dstr);
     }
   } else {
     sass_context *ctx = (sass_context*)cptr;

test/source_map_spec.js
@@ -1,0 +1,24 @@
+/*jshint multistr:true */
+var sass = require('../sass');
+var assert = require('assert');
+
+var sampleFilename = require('path').resolve(__dirname, 'sample.scss');
+
+describe("compile source maps", function() {
+  it("should compile file with source map URL", function(done) {
+    var mapFileName = 'sample.css.map';
+    sass.render({
+      file: sampleFilename,
+      sourceComments: 'map',
+      sourceMap: mapFileName,
+      success: function (css, map) {
+        assert.ok(css.indexOf('sourceMappingURL=' + mapFileName) !== -1);
+        assert.ok(map.indexOf('sample.scss') !== -1);
+        done();
+      },
+      error: function (error) {
+        done(error);
+      }
+    });
+  });
+});