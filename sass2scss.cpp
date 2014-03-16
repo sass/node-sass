09dc6d7be7567dbeceb059e75ee945fa5109cfdd
Marcel Greter <marcel.greter@ocbnet.ch>
2014-03-16 09:31:26 +0100

    Converts into function that takes a string as argument

--- sass2scss.cpp
+++ sass2scss.cpp
@@ -1,40 +1,39 @@
 // include library
+#include <sstream>
 #include <iostream>
 
 // using std::string
 using namespace std;
 
-// pretty print option
-// set by command line
-int pretty = 0;
-
-// whitespace buffer
-string whitespace;
-
-// concatenate comma lists
-bool comma = false;
-
-// comment context
-string comment = "";
-
-// indent level
-int level = 0;
-
-// indent strings
-// length should be dynamic
-// but my c++ is very rusty
-string indents[255];
+struct converter
+{
+	// indent level
+	int level;
+	// pretty print option
+	// set by command line
+	int pretty;
+	// concatenate comma lists
+	bool comma;
+	// comment context
+	string comment;
+	// whitespace buffer
+	string whitespace;
+	// indent strings
+	// length should be dynamic
+	// but my c++ is very rusty
+	string indents[255];
+};
 
 // helper function
-static string closer ()
+static string closer (converter& converter)
 {
-	return pretty > 0 ? "\n" + indents[level] + "}" : " }";
+	return converter.pretty > 0 ? "\n" + converter.indents[converter.level] + "}" : " }";
 }
 
 // helper function
-static string opener ()
+static string opener (converter& converter)
 {
-	return pretty > 1 ? "\n" + indents[level] + "{" : " {";
+	return converter.pretty > 1 ? "\n" + converter.indents[converter.level] + "{" : " {";
 }
 
 // flush whitespace and
@@ -42,28 +41,33 @@ static string opener ()
 // but only print additional
 // chars and buffer whitespace
 // ***************************************************************************************
-void flush (ostream& os, string& str)
+string flush (string& str, converter& converter)
 {
+	string scss = "";
 	// print whitespace
-	os << whitespace;
+	scss += converter.whitespace;
 	// reset whitespace
-	whitespace = "";
+	converter.whitespace = "";
 	// remove newlines
 	int pos = str.find_last_not_of("\n\r");
 	string lfs = str.substr(pos + 1);
 	str = str.substr(0, pos + 1);
 	// getline discharged newline
-	whitespace += lfs + "\n";
+	converter.whitespace += lfs + "\n";
 	// print text
-	os << str;
+	scss += str;
+	// return string
+	return scss;
 }
 
 // process a line of the sass text
-void process (ostream& os, string& str, bool final = false)
+string process (string& sass, converter& converter, bool final = false)
 {
 
+	string scss = "";
+
 	// get postion of first meaningfull char
-	int pos_left = str.find_first_not_of(" \t\n\v\f\r");
+	int pos_left = sass.find_first_not_of(" \t\n\v\f\r");
 
 	// special case for final run
 	if (final) pos_left = 0;
@@ -72,103 +76,103 @@ void process (ostream& os, string& str, bool final = false)
 	if (pos_left == string::npos)
 	{
 		// add whitespace
-		whitespace += str;
+		converter.whitespace += sass;
 	}
 	// have meaningfull first char
 	else
 	{
 
 		// store indentation string
-		string indent = str.substr(0, pos_left);
+		string indent = sass.substr(0, pos_left);
 
 		// special case for multiline comment, when the next
 		// line is on the same indentation as the actual comment
 		// I assume that this means we should close the comment node
-		if (comment != "" && indent.length() <= indents[level].length())
+		if (converter.comment != "" && indent.length() <= converter.indents[converter.level].length())
 		{
 			// close comment
-			os << " */";
+			scss += " */";
 			// unset flag
-			comment = "";
+			converter.comment = "";
 		}
 		// current line has less or same indentation
-		else if (level > 0 && indent.length() <= indents[level].length())
+		else if (converter.level > 0 && indent.length() <= converter.indents[converter.level].length())
 		{
 			// add semicolon if not in concat mode
-			if (comment == "" && comma == false) os << ";";
+			if (converter.comment == "" && converter.comma == false) scss += ";";
 		}
 
 		// make sure we close every "higher" block
-		while (indent.length() < indents[level].length())
+		while (indent.length() < converter.indents[converter.level].length())
 		{
 			// reset string
-			indents[level] == "";
+			converter.indents[converter.level] == "";
 			// close block
-			level --;
+			converter.level --;
 			// print closer
-			if (comment == "")
-			{ os << closer(); }
-			else { os << " */"; }
+			if (converter.comment == "")
+			{ scss += closer(converter); }
+			else { scss += " */"; }
 			// reset comment
-			comment = "";
+			converter.comment = "";
 		}
 
 		// check if we have sass property syntax
-		if (str.substr(pos_left, 1) == ":")
+		if (sass.substr(pos_left, 1) == ":")
 		{
 			// get postion of first meaningfull char
-			int pos_value = str.find_first_of(" \t\n\v\f\r", pos_left);
+			int pos_value = sass.find_first_of(" \t\n\v\f\r", pos_left);
 			// create new string by interchanging the colon sign for property and value
-			str = indent + str.substr(pos_left + 1, pos_value - pos_left - 1) + ":" + str.substr(pos_value);
+			sass = indent + sass.substr(pos_left + 1, pos_value - pos_left - 1) + ":" + sass.substr(pos_value);
 		}
 
 		// replace some specific sass shorthand directives
-		else if (str.substr(pos_left, 1) == "=") { str = indent + "@mixin " + str.substr(pos_left + 1); }
-		else if (str.substr(pos_left, 1) == "+") { str = indent + "@include " + str.substr(pos_left + 1); }
+		else if (sass.substr(pos_left, 1) == "=") { sass = indent + "@mixin " + sass.substr(pos_left + 1); }
+		else if (sass.substr(pos_left, 1) == "+") { sass = indent + "@include " + sass.substr(pos_left + 1); }
 		// add quotes for import if needed
-		else if (str.substr(pos_left, 7) == "@import")
+		else if (sass.substr(pos_left, 7) == "@import")
 		{
-			int pos_import = str.find_first_of(" \t\n\v\f\r", pos_left + 7);
-			int pos_quote = str.find_first_not_of(" \t\n\v\f\r", pos_import);
-			if (str.substr(pos_quote, 1) != "\"")
+			int pos_import = sass.find_first_of(" \t\n\v\f\r", pos_left + 7);
+			int pos_quote = sass.find_first_not_of(" \t\n\v\f\r", pos_import);
+			if (sass.substr(pos_quote, 1) != "\"")
 			{
-				int pos_end = str.find_last_not_of(" \t\n\v\f\r");
-				str = str.substr(0, pos_quote) + "\"" + str.substr(pos_quote, pos_end - pos_quote + 1) + "\"";
+				int pos_end = sass.find_last_not_of(" \t\n\v\f\r");
+				sass = sass.substr(0, pos_quote) + "\"" + sass.substr(pos_quote, pos_end - pos_quote + 1) + "\"";
 			}
 		}
 
 		// check if current line starts a comment
-		string open = str.substr(pos_left, 2);
+		string open = sass.substr(pos_left, 2);
 
 		// current line has more indentation
-		if (indent.length() > indents[level].length())
+		if (indent.length() > converter.indents[converter.level].length())
 		{
 			// not in comment mode
-			if (comment == "")
+			if (converter.comment == "")
 			{
 				// print block opener
-				os << opener();
+				scss += opener(converter);
 				// open new block
-				level ++;
+				converter.level ++;
 				// store block indentation
-				indents[level] = indent;
+				converter.indents[converter.level] = indent;
 			}
 			// open new block if comment is opening
 			// be smart and only require the same indentation
 			// level as the comment node itself, plus one char
-			if (comment == "/*" && comment == "//")
+			if (converter.comment == "/*" && converter.comment == "//")
 			{
 				// only increase indentation minimaly
 				// this will accept everything that is more
 				// indented than the opening comment line
-				indents[level] = indents[level] + ' ';
+				converter.indents[converter.level] = converter.indents[converter.level] + ' ';
 				// count new block
-				level ++;
+				converter.level ++;
 			}
 			// set comment to current indent
 			// multiline comments must be indented
 			// indicates multiline if not eq "*"
-			if (comment != "") comment = indent;
+			if (converter.comment != "") converter.comment = indent;
 		}
 
 		// line is opening a new comment
@@ -176,36 +180,36 @@ void process (ostream& os, string& str, bool final = false)
 		{
 			// force single line comments
 			// into a correct css comment
-			str[pos_left + 1] = '*';
+			sass[pos_left + 1] = '*';
 			// close previous comment
-			if (comment != "") os << " */";;
+			if (converter.comment != "") scss += " */";;
 			// remove indentation from previous comment
-			if (comment == "//")
+			if (converter.comment == "//")
 			{
 				// reset string
-				indents[level] == "";
+				converter.indents[converter.level] == "";
 				// close block
-				level --;
+				converter.level --;
 			}
 			// set comment flag
-			comment = open;
+			converter.comment = open;
 		}
 
 		// flush line
-		flush(os, str);
+		scss += flush(sass, converter);
 
 		// get postion of last meaningfull char
-		int pos_right = str.find_last_not_of(" \t\n\v\f\r");
+		int pos_right = sass.find_last_not_of(" \t\n\v\f\r");
 
 		// check for invalid result
 		if (pos_right != string::npos)
 		{
 
 			// get the last meaningfull char
-			string close = str.substr(pos_right, 1);
+			string close = sass.substr(pos_right, 1);
 
 			// check if next line should be concatenated
-			comma = comment == "" && close == ",";
+			converter.comma = converter.comment == "" && close == ",";
 
 			// check if we have more than
 			// one meaningfull char
@@ -213,16 +217,16 @@ void process (ostream& os, string& str, bool final = false)
 			{
 
 				// get the last two chars from string
-				string close = str.substr(pos_right - 1, 2);
+				string close = sass.substr(pos_right - 1, 2);
 				// is comment node closed expicitly
 				if (close == "*/")
 				{
 					// close implicit comment
-					comment = "";
+					converter.comment = "";
 					// reset string
-					indents[level] == "";
+					converter.indents[converter.level] == "";
 					// close block
-					level --;
+					converter.level --;
 				}
 
 			}
@@ -234,10 +238,41 @@ void process (ostream& os, string& str, bool final = false)
 	}
 	// EO have meaningfull chars from start
 
+	// return scss
+	return scss;
+}
+
+string sass2scss (string sass, int pretty)
+{
+
+	string out = "";
+	char delims = '\n';
+	stringstream stream(sass);
+	string line;
+
+	converter converter;
+	converter.pretty = pretty;
+	converter.level = 0;
+	converter.whitespace = "";
+	converter.indents[0] = "";
+
+	while(std::getline(stream, line, '\n')){
+		out += process(line, converter);
+	}
+
+	string end = "";
+	out += process(end, converter, true);
+
+	return out;
+
 }
 
 int main (int argc, char** argv)
 {
+
+	// pretty print option
+	// set by command line
+	int pretty = 0;
 
 	// process command line arguments
 	for (int i = 0; i < argc; ++i)
@@ -250,9 +285,8 @@ int main (int argc, char** argv)
 	// declare variable
 	string line;
 
-	// init variables
-	whitespace = "";
-	indents[0] = "";
+	// accumulate for test
+	string sass = "";
 
 	// read from stdin
 	while (cin)
@@ -260,13 +294,12 @@ int main (int argc, char** argv)
 		// read a line
 		getline(cin, line);
 		// process the line
-		process(cout, line);
+		sass += line + "\n";
 		// break if at end of file
 		if (cin.eof()) break;
 	};
 
-	// flush buffer
-	string end = "";
-	process(cout, end, true);
+	// print the resulting scss
+	cout << sass2scss (sass, pretty);
 
 }
