4761a5649deeb20b7b79bb092b1a85dd0024927d
Marcel Greter <marcel.greter@ocbnet.ch>
2014-04-15 12:53:35 +0200

    Fixes double conversion from sass files

--- file.cpp
+++ file.cpp
@@ -161,13 +161,7 @@ namespace Sass {
       }
       for(int i=0; i<extension.size();++i)
         extension[i] = tolower(extension[i]);
-      if (extension == ".sass" && contents != 0) {
-        char * converted = Sass::sass2scss(contents, SASS2SCSS_PRETTIFY_1);
-        delete[] contents; // free the indented contents
-        return converted; // should be freed by caller
-      } else {
-        return contents;
-      }
+      return contents;
     }
 
     char* read_file(string path)
