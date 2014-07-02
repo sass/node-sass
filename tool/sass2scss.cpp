ad1e72ec6e1fa3248a6551f4d3ba945ac252131a
Marcel Greter <marcel.greter@ocbnet.ch>
2014-07-02 21:56:54 +0200

    Adds comment options for command line tool

--- tool/sass2scss.cpp
+++ tool/sass2scss.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <sstream>
 #include <iostream>
+#include <stdlib.h>
 
 #include "sass2scss.h"
 
@@ -18,12 +19,25 @@ int main (int argc, char** argv)
 	// set by command line
 	int pretty = 0;
 
+	// bitwise options
+	// set by command line
+	int options = 0;
+
 	// process command line arguments
-	for (int i = 0; i < argc; ++i)
+	for (int i = 1; i < argc; ++i)
 	{
-		// only handle prettyfying option
+		// handle prettyfying option
 		if ("-p" == string(argv[i])) pretty ++;
-		if ("--pretty" == string(argv[i])) pretty ++;
+		else if ("--pretty" == string(argv[i])) pretty ++;
+		// handle other bitwise options (comment related)
+		else if ("-k" == string(argv[i])) options |= SASS2SCSS_KEEP_COMMENT;
+		else if ("--keep" == string(argv[i])) options |= SASS2SCSS_KEEP_COMMENT;
+		else if ("-s" == string(argv[i])) options |= SASS2SCSS_STRIP_COMMENT;
+		else if ("--strip" == string(argv[i])) options |= SASS2SCSS_STRIP_COMMENT;
+		else if ("-c" == string(argv[i])) options |= SASS2SCSS_CONVERT_COMMENT;
+		else if ("--convert" == string(argv[i])) options |= SASS2SCSS_CONVERT_COMMENT;
+		// error out with a generic error message (keep it simple for now)
+		else { cerr << "Unknown command line option " << argv[i] << endl; exit(EXIT_FAILURE); }
 	}
 
 	// declare variable
@@ -43,7 +57,11 @@ int main (int argc, char** argv)
 		if (cin.eof()) break;
 	};
 
+	// simple assertion for valid value
+	if (pretty > SASS2SCSS_PRETTIFY_3)
+	{ pretty = SASS2SCSS_PRETTIFY_3; }
+
 	// print the resulting scss
-	cout << Sass::sass2scss (sass, pretty);
+	cout << Sass::sass2scss (sass, pretty | options);
 
 }
