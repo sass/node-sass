535b5249c9b2b701f7e56578ad7f76fb66b5da0e
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-29 12:21:55 +0100

    Cleanup tests

---
+++ test/spec.js
@@ -0,0 +1,57 @@
+var assert = require('assert'),
+    fs = require('fs'),
+    exists = fs.existsSync,
+    path = require('path'),
+    read = fs.readFileSync,
+    sass = require('../sass'),
+    util = require('./util');
+
+describe('spec', function () {
+  var suites = util.getSuites();
+
+  describe('test/sass-spec directory', function() {
+    it('should be a cloned into place', function(done) {
+      fs.exists(path.join(__dirname, 'sass-spec'), function (exists) {
+        if (!exists) {
+          throw new Error([
+            'test/sass-spec directory missing. Please clone it into place by',
+            'executing `git submodule update --init --recursive test/sass-spec`',
+            'from the project\'s root directory.'
+          ].join(' '));
+        }
+
+        assert(exists);
+        done();
+      });
+    });
+  });
+
+  Object.keys(suites).forEach(function(suite) {
+    var tests = Object.keys(suites[suite]);
+
+    describe(suite, function () {
+      tests.forEach(function(test) {
+        var t = suites[suite][test];
+
+        if (exists(t.src)) {
+          it(test, function(done) {
+            var expected = util.normalize(read(t.expected, 'utf8'));
+
+            sass.render({
+              file: t.src,
+              includePaths: t.paths,
+              success: function(css) {
+                assert.equal(util.normalize(css), expected);
+                done();
+              },
+              error: function(err) {
+                assert(!err);
+                done();
+              }
+            });
+          });
+        }
+      });
+    });
+  });
+});
---
+++ test/util/index.js
@@ -0,0 +1,54 @@
+var fs = require('fs'),
+    join = require('path').join,
+    spec = join(__dirname, '..', 'sass-spec', 'spec');
+
+/**
+ * Normalize CSS
+ *
+ * @param {String} css
+ * @api public
+ */
+
+module.exports.normalize = function(str) {
+  return str.replace(/\s+/g, '').replace('{', '{\n').replace(';', ';\n');
+};
+
+/**
+ * Get test suites
+ *
+ * @api public
+ */
+
+module.exports.getSuites = function() {
+  var ret = {};
+  var suites = fs.readdirSync(spec);
+  var ignoreSuites = [
+    'libsass-todo-issues',
+    'libsass-todo-tests'
+  ];
+
+  suites.forEach(function(suite) {
+    if (ignoreSuites.indexOf(suite) !== -1) {
+      return;
+    }
+
+    var suitePath = join(spec, suite);
+    var tests = fs.readdirSync(suitePath);
+
+    ret[suite] = {};
+
+    tests.forEach(function(test) {
+      var testPath = join(suitePath, test);
+
+      ret[suite][test] = {};
+      ret[suite][test].src = join(testPath, 'input.scss');
+      ret[suite][test].expected = join(testPath, 'expected_output.css');
+      ret[suite][test].paths = [
+        testPath,
+        join(testPath, 'sub')
+      ];
+    });
+  });
+
+  return ret;
+};
