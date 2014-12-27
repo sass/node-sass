7021663a18f4670852e039a832073feb3c99ff30
Marcel Greter <marcel.greter@ocbnet.ch>
2014-12-27 03:29:03 +0100

    Add a check to really only remove backslashes

--- inspect.cpp
+++ inspect.cpp
@@ -698,7 +698,7 @@ namespace Sass {
     t.reserve(s.length()-2);
     for (size_t i = 1, L = s.length()-1; i < L; ++i) {
       // if we see a quote, we need to remove the preceding backslash from t
-      if (s[i] == q) t.erase(t.length()-1);
+      if (s[i-1] == '\\' && s[i] == q) t.erase(t.length()-1);
       t.push_back(s[i]);
     }
     return t;
