f94edf2e808e3f35158a35ea0b616633129094e3
Marcel Greter <marcel.greter@ocbnet.ch>
2015-01-29 20:03:24 +0100

    Add @charset support to sass2scss
    Fixes https://github.com/sass/libsass/issues/845
    https://github.com/mgreter/sass2scss/releases/tag/v1.0.3

--- sass2scss.cpp
+++ sass2scss.cpp
@@ -561,11 +561,12 @@ namespace Sass
 
 			}
 
-			// terminate warn and debug statements immediately
+			// terminate some statements immediately
 			else if (
 				sass.substr(pos_left, 5) == "@warn" ||
 				sass.substr(pos_left, 6) == "@debug" ||
-				sass.substr(pos_left, 6) == "@error"
+				sass.substr(pos_left, 6) == "@error" ||
+				sass.substr(pos_left, 8) == "@charset"
 			) { sass = indent + sass.substr(pos_left); }
 			// replace some specific sass shorthand directives (if not fallowed by a white space character)
 			else if (sass.substr(pos_left, 1) == "=" && sass.find_first_of(SASS2SCSS_FIND_WHITESPACE, pos_left) != pos_left + 1)
