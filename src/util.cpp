fb79a208b318f1cca6833853831c5edca76c7695
Marcin Cieslak <saper@saper.info>
2015-03-28 02:55:59 +0000

    Use NULL instead of C++ nullptr

--- src/util.cpp
+++ src/util.cpp
@@ -107,7 +107,7 @@ namespace Sass {
 
           // convert the extracted hex string to code point value
           // ToDo: Maybe we could do this without creating a substring
-          uint32_t cp = strtol(s.substr (i + 1, len - 1).c_str(), 0, 16);
+          uint32_t cp = strtol(s.substr (i + 1, len - 1).c_str(), NULL, 16);
 
           if (cp == 0) cp = 0xFFFD;
 
@@ -401,7 +401,7 @@ namespace Sass {
 
           // convert the extracted hex string to code point value
           // ToDo: Maybe we could do this without creating a substring
-          uint32_t cp = strtol(s.substr (i + 1, len - 1).c_str(), 0, 16);
+          uint32_t cp = strtol(s.substr (i + 1, len - 1).c_str(), NULL, 16);
 
           if (s[i + len] == ' ') ++ len;
 
