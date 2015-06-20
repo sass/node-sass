3bd1ccc87cf3e7c4c93a264798f53993b9407f6c
Marcin Cieslak <saper@saper.info>
2015-06-20 23:47:17 +0000

    Add live tracing facility
    Set NODESASS_TRACE enviroment variable
    to any value to get lots of output.

---
+++ src/debug.h
@@ -0,0 +1,38 @@
+#ifndef NODE_SASS_DEBUG_H
+#define NODE_SASS_DEBUG_H
+
+#include <sstream>
+
+enum TLogLevel {logINFO, logTRACE};
+static TLogLevel LogReportingLevel = getenv("NODESASS_TRACE") ? logTRACE : logINFO;
+class Log
+{
+public:
+  Log();
+  virtual ~Log();
+  std::ostringstream& Get(TLogLevel level, void *p, const char *f, const char *filen, int lineno);
+  std::ostringstream& Get(TLogLevel level, const char *f, const char *filen, int lineno);
+public:
+protected:
+  std::ostringstream os;
+private:
+  Log(const Log&);
+  Log& operator =(const Log&);
+private:
+  TLogLevel messageLevel;
+};
+
+// Visual Studio 2013 does not like __func__
+#if _MSC_VER < 1900
+#define __func__ __FUNCTION__
+#endif
+
+#define TRACE() \
+  if (logTRACE > LogReportingLevel) ; \
+  else Log().Get(logTRACE, __func__, __FILE__, __LINE__)
+
+#define TRACEINST(obj) \
+  if (logTRACE > LogReportingLevel) ; \
+  else Log().Get(logTRACE, (obj), __func__, __FILE__, __LINE__)
+
+#endif
