8d48713f70258769e72ad464d755de58df6a9451
Marcin Cieslak <saper@saper.info>
2015-06-21 00:46:53 +0000

    Trace in/out values for custom functions
    Introduce one true way to print contents
    of the SassValue union.

--- src/custom_function_bridge.cpp
+++ src/custom_function_bridge.cpp
@@ -3,10 +3,12 @@
 #include "custom_function_bridge.h"
 #include "sass_types/factory.h"
 #include "sass_types/value.h"
+#include "debug.h"
 
 Sass_Value* CustomFunctionBridge::post_process_return_value(v8::Local<v8::Value> val) const {
   SassTypes::Value *v_;
   if ((v_ = SassTypes::Factory::unwrap(val))) {
+    TRACEINST(&val) << " CustomFunctionBridge: unwrapping custom function return value...";
     return v_->get_sass_value();
   } else {
     return sass_make_error("A SassValue object was expected.");
@@ -17,6 +19,7 @@ std::vector<v8::Local<v8::Value>> CustomFunctionBridge::pre_process_args(std::ve
   std::vector<v8::Local<v8::Value>> argv = std::vector<v8::Local<v8::Value>>();
 
   for (void* value : in) {
+    TRACEINST(&value) << " CustomFunctionBridge: wrapping custom function parameters...";
     argv.push_back(SassTypes::Factory::create(static_cast<Sass_Value*>(value))->get_js_object());
   }
 
