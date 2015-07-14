mgreter/libsass

Implement initial parts for reference combinators

eval.cpp
@@ -1397,8 +1397,10 @@ namespace Sass {
     bool parentized = false;
     Complex_Selector* tail = s->tail();
     Compound_Selector* head = s->head();
+    String* reference = s->reference();
     Complex_Selector::Combinator combinator = s->combinator();
     Selector_List* sl = new (ctx.mem) Selector_List(s->pstate());
+    if (reference) reference = (String*) reference->perform(this);
 
     if (head) {
       // check if we have a parent selector reference (expands to list)
@@ -1420,6 +1422,7 @@ namespace Sass {
                   cp = new (ctx.mem) Complex_Selector(s->pstate());
                   cp->head(head); cp->tail(tt);
                   cp->combinator(combinator);
+                  cp->reference(reference);
                   last->tail(cp);
                 } else {
                   last->tail(tt);
@@ -1435,6 +1438,7 @@ namespace Sass {
               Complex_Selector* ns = (*pr)[n]->cloneFully(ctx);
               Complex_Selector* last = ns->last();
               ns->combinator(combinator);
+              ns->reference(reference);
               for (size_t i = 1, iL = head->size(); i < iL; ++i) {
                 // add simple selectors
                 *last->head() << (*head)[i];

listize.cpp
@@ -45,6 +45,9 @@ namespace Sass {
       if (hh) *l << hh;
     }
 
+    To_String to_string;
+    string reference = ! sel->reference() ? ""
+      : sel->reference()->perform(&to_string);
     switch(sel->combinator())
     {
       case Complex_Selector::PARENT_OF:
@@ -52,6 +55,9 @@ namespace Sass {
       break;
       case Complex_Selector::ADJACENT_TO:
         *l << new (ctx.mem) String_Constant(sel->pstate(), "+");
+      break;
+      case Complex_Selector::REFERENCE:
+        *l << new (ctx.mem) String_Constant(sel->pstate(), "/" + reference + "/");
       break;
       case Complex_Selector::PRECEDES:
         *l << new (ctx.mem) String_Constant(sel->pstate(), "~");

sass/node-sass

eval.cpp
@@ -1402,8 +1402,11 @@ namespace Sass {
     bool parentized = false;
     Complex_Selector* tail = s->tail();
     Compound_Selector* head = s->head();
+    String* reference = s->reference();
     Complex_Selector::Combinator combinator = s->combinator();
     Selector_List* sl = new (ctx.mem) Selector_List(s->pstate());
+    if (reference) reference = (String*) reference->perform(this);
+
     if (head) {
       // check if we have a parent selector reference (expands to list)
       if (head->length() > 1 && dynamic_cast<Parent_Selector*>((*head)[0])) {
@@ -1418,6 +1421,7 @@ namespace Sass {
             for (size_t i = 1, L = head->length(); i < L; ++i) *lst_h << (*head)[i];
             lst_t->tail(tail); // now connect old tail back to new intermediate
             lst_t->combinator(combinator); // and dont forget the combinator
+            lst_t->reference(reference);
             // if (s->has_line_feed()) lst_t->has_line_feed(true); // and dont forget the combinator
           }
           return ns;
@@ -1450,6 +1454,7 @@ namespace Sass {
                 lst->tail(ins);
               } else {
                 lst->combinator(combinator);
+                lst->reference(reference);
               }
             }
             if (s->has_line_feed()) (*ns)[n]->has_line_feed(true);

listize.cpp
@@ -45,6 +45,9 @@ namespace Sass {
       if (hh) *l << hh;
     }
 
+    To_String to_string;
+    string reference = ! sel->reference() ? ""
+      : sel->reference()->perform(&to_string);
     switch(sel->combinator())
     {
       case Complex_Selector::PARENT_OF:
@@ -52,6 +55,9 @@ namespace Sass {
       break;
       case Complex_Selector::ADJACENT_TO:
         *l << new (ctx.mem) String_Quoted(sel->pstate(), "+");
+      break;
+      case Complex_Selector::REFERENCE:
+        *l << new (ctx.mem) String_Quoted(sel->pstate(), "/" + reference + "/");
       break;
       case Complex_Selector::PRECEDES:
         *l << new (ctx.mem) String_Quoted(sel->pstate(), "~");