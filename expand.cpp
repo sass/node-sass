d4cb91cf3a9f22d8ade4c4b9612907c95b6c57f7
Marcel Greter <marcel.greter@ocbnet.ch>
2015-07-16 22:01:59 +0200

    Evaluate media_queries for import statements
    Fixes https://github.com/sass/libsass/issues/1322

--- expand.cpp
+++ expand.cpp
@@ -279,7 +279,10 @@ namespace Sass {
   Statement* Expand::operator()(Import* imp)
   {
     Import* result = new (ctx.mem) Import(imp->pstate());
-    result->media_queries(imp->media_queries());
+    if (imp->media_queries()) {
+      Expression* ex = imp->media_queries()->perform(&eval);
+      result->media_queries(dynamic_cast<List*>(ex));
+    }
     for ( size_t i = 0, S = imp->urls().size(); i < S; ++i) {
       result->urls().push_back(imp->urls()[i]->perform(&eval));
     }
