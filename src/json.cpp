b7835ab0f1695c352ee8f72227cee29380166556
saper <saper@saper.info>
2015-08-13 16:51:28 +0200

    Use C99 snprintf() for VS2013 or older

--- src/json.cpp
+++ src/json.cpp
@@ -35,34 +35,12 @@
 #include <string.h>
 
 #if defined(_MSC_VER) && _MSC_VER < 1900
-
 #include <stdarg.h>
-#define snprintf c99_snprintf
-
-inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
-{
-    int count = -1;
-
-    if (size != 0)
-        count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
-    if (count == -1)
-        count = _vscprintf(format, ap);
-
-    return count;
-}
-
-inline int c99_snprintf(char* str, size_t size, const char* format, ...)
-{
-    int count;
-    va_list ap;
-
-    va_start(ap, format);
-    count = c99_vsnprintf(str, size, format, ap);
-    va_end(ap);
-
-    return count;
-}
-#endif // _MSC_VER
+#ifdef snprintf
+#undef snprintf
+#endif
+extern "C" int snprintf(char *, size_t, const char *, ...);
+#endif
 
 #define out_of_memory() do {                    \
     fprintf(stderr, "Out of memory.\n");    \
