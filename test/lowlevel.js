ef5c28fde66f4c42af7a30a75f4a9388715b8577
Marcin Cieslak <saper@saper.info>
2015-08-20 22:15:11 +0000

    Raise an error when no string/file provided
    Intended to fix https://github.com/sass/node-sass/issues/924

--- test/lowlevel.js
+++ test/lowlevel.js
@@ -12,6 +12,90 @@ describe('lowlevel', function() {
     done();
   });
 
+  it('data context with options.data not provided', function(done) {
+    var options =  { 
+      /* data: */
+      sourceComments: false,
+      file: null,
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      indentWidth: 2,
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+    binding.renderSync(options);
+    assert(/Data context created without a source string/.test(options.result.error), 
+          'Should fail with error message "Data context created without a source string"');
+    done();
+  });
+
+  it('data context with both options.data and options.file not provided', function(done) {
+    var options =  { 
+      /* data: */
+      sourceComments: false,
+      /* file: null, */
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      indentWidth: 2,
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+    binding.renderSync(options);
+    assert(/Data context created without a source string/.test(options.result.error), 
+          'Should fail with error message "Data context created without a source string"');
+    done();
+  });
+
+  it('file context with both options.data and options.file not provided', function(done) {
+    var options =  { 
+      /* data: */
+      sourceComments: false,
+      /* file: null, */
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      indentWidth: 2,
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+    binding.renderFileSync(options);
+    assert(/File context created without an input path/.test(options.result.error), 
+          'Should fail with error message "File context created without an input path"');
+    done();
+  });
+
+  it('file context with options.file not provided, options.data given', function(done) {
+    var options =  { 
+      data: 'div { width: 10px; } ',
+      sourceComments: false,
+      /* file: null, */
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      indentWidth: 2,
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+    binding.renderFileSync(options);
+    assert(/File context created without an input path/.test(options.result.error), 
+          'Should fail with error message "File context created without an input path"');
+    done();
+  });
+
   it('fail with options.result not provided', function(done) {
     var options =  { data: 'div { width: 10px; } ',
       sourceComments: false,
@@ -96,7 +180,7 @@ describe('lowlevel', function() {
   });
 
   it('options.indentWidth not provided', function(done) {
-    var options =  { data: 'div { width: 10px; } ',
+    var options =  { data: 'div { width: 10px; }',
       sourceComments: false,
       file: null,
       outFile: null,
@@ -110,8 +194,49 @@ describe('lowlevel', function() {
       result: { stats: {} } };
 
       binding.renderSync(options);
+      assert(options.result.css);
     done();
   });
 
+  it('empty data string', function(done) {
+    var options =  { data: '',
+      sourceComments: false,
+      file: null,
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      /* indentWidth */
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+      binding.renderSync(options);
+      assert(/empty source string/.test(options.result.error), 
+          'Should fail with error message "Data context created with empty source string"');
+    done();
+  });
+
+
+  it('empty file string', function(done) {
+    var options =  { 
+      sourceComments: false,
+      file: '',
+      outFile: null,
+      includePaths: '',
+      precision: 5,
+      sourceMap: null,
+      style: 0,
+      /* indentWidth */
+      indentType: 0,
+      linefeed: '\n',
+      result: { stats: {} } };
+
+      binding.renderFileSync(options);
+      assert(/empty input path/.test(options.result.error), 
+          'Should fail with error message "File context created with empty input path"');
+    done();
+  });
 
 }); // lowlevel
