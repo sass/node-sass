sass/node-sass

commit 0e32dca5112534a74fe12348ad8ddb567100a5a3

Refactor selector compare and unify functions

src/ast.hpp
@@ -128,7 +128,7 @@ namespace Sass {
     virtual bool is_invisible() const { return false; }
     static string type_name() { return ""; }
     virtual bool is_false() { return false; }
-    virtual bool operator==( Expression& rhs) const { return false; }
+    virtual bool operator== (const Expression& rhs) const { return false; }
     virtual void set_delayed(bool delayed) { is_delayed(delayed); }
     virtual size_t hash() { return 0; }
   };
@@ -142,8 +142,8 @@ namespace Sass {
           bool d = false, bool e = false, bool i = false, Concrete_Type ct = NONE)
     : Expression(pstate, d, e, i, ct)
     { }
-    virtual bool operator== (Expression& rhs) const = 0;
-    virtual bool operator== (Expression* rhs) const = 0;
+    virtual bool operator== (const Expression& rhs) const = 0;
+    virtual bool operator== (const Expression* rhs) const = 0;
     virtual string to_string(bool compressed = false, int precision = 5) const = 0;
   };
 }
@@ -218,6 +218,12 @@ namespace Sass {
     vector<T>& elements() { return elements_; }
     const vector<T>& elements() const { return elements_; }
     vector<T>& elements(vector<T>& e) { elements_ = e; return elements_; }
+
+    typename vector<T>::iterator end() { return elements_.end(); }
+    typename vector<T>::iterator begin() { return elements_.begin(); }
+    typename vector<T>::const_iterator end() const { return elements_.end(); }
+    typename vector<T>::const_iterator begin() const { return elements_.begin(); }
+
   };
   template <typename T>
   inline Vectorized<T>::~Vectorized() { }
@@ -812,8 +818,8 @@ namespace Sass {
       is_delayed(delayed);
     }
 
-    virtual bool operator==(Expression& rhs) const;
-    virtual bool operator==(Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -846,8 +852,8 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -894,15 +900,15 @@ namespace Sass {
       left()->set_delayed(delayed);
       is_delayed(delayed);
     }
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Binary_Expression& m = dynamic_cast<Binary_Expression&>(rhs);
+        const Binary_Expression* m = dynamic_cast<const Binary_Expression*>(&rhs);
         if (m == 0) return false;
-        return type() == m.type() &&
-               left() == m.left() &&
-               right() == m.right();
+        return type() == m->type() &&
+               left() == m->left() &&
+               right() == m->right();
       }
       catch (std::bad_cast&)
       {
@@ -944,14 +950,14 @@ namespace Sass {
         default: return "invalid"; break;
       }
     }
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Unary_Expression& m = dynamic_cast<Unary_Expression&>(rhs);
+        const Unary_Expression* m = dynamic_cast<const Unary_Expression*>(&rhs);
         if (m == 0) return false;
-        return type() == m.type() &&
-               operand() == m.operand();
+        return type() == m->type() &&
+               operand() == m->operand();
       }
       catch (std::bad_cast&)
       {
@@ -988,13 +994,13 @@ namespace Sass {
       }
     }
 
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Argument& m = dynamic_cast<Argument&>(rhs);
-        if (!(m && name() == m.name())) return false;
-        return *value() == *m.value();
+        const Argument* m = dynamic_cast<const Argument*>(&rhs);
+        if (!(m && name() == m->name())) return false;
+        return *value() == *m->value();
       }
       catch (std::bad_cast&)
       {
@@ -1053,15 +1059,15 @@ namespace Sass {
     : Expression(pstate), name_(n), arguments_(args), cookie_(0), hash_(0)
     { concrete_type(STRING); }
 
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Function_Call& m = dynamic_cast<Function_Call&>(rhs);
-        if (!(m && name() == m.name())) return false;
-        if (!(m && arguments()->length() == m.arguments()->length())) return false;
+        const Function_Call* m = dynamic_cast<const Function_Call*>(&rhs);
+        if (!(m && name() == m->name())) return false;
+        if (!(m && arguments()->length() == m->arguments()->length())) return false;
         for (size_t i =0, L = arguments()->length(); i < L; ++i)
-          if (!((*arguments())[i] == (*m.arguments())[i])) return false;
+          if (!((*arguments())[i] == (*m->arguments())[i])) return false;
         return true;
       }
       catch (std::bad_cast&)
@@ -1107,12 +1113,12 @@ namespace Sass {
     : Expression(pstate), name_(n)
     { }
 
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Variable& e = dynamic_cast<Variable&>(rhs);
-        return e && name() == e.name();
+        const Variable* e = dynamic_cast<const Variable*>(&rhs);
+        return e && name() == e->name();
       }
       catch (std::bad_cast&)
       {
@@ -1146,12 +1152,12 @@ namespace Sass {
       hash_(0)
     { }
 
-    virtual bool operator==(Expression& rhs) const
+    virtual bool operator==(const Expression& rhs) const
     {
       try
       {
-        Textual& e = dynamic_cast<Textual&>(rhs);
-        return e && value() == e.value() && type() == e.type();
+        const Textual* e = dynamic_cast<const Textual*>(&rhs);
+        return e && value() == e->value() && type() == e->type();
       }
       catch (std::bad_cast&)
       {
@@ -1206,10 +1212,10 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator< (Number& rhs) const;
-    virtual bool operator< (Number* rhs) const;
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator< (const Number& rhs) const;
+    virtual bool operator< (const Number* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -1245,8 +1251,8 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -1261,8 +1267,8 @@ namespace Sass {
     Custom_Error(ParserState pstate, string msg)
     : Value(pstate), message_(msg)
     { concrete_type(C_ERROR); }
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
     ATTACH_OPERATIONS()
   };
@@ -1276,8 +1282,8 @@ namespace Sass {
     Custom_Warning(ParserState pstate, string msg)
     : Value(pstate), message_(msg)
     { concrete_type(C_WARNING); }
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
     ATTACH_OPERATIONS()
   };
@@ -1306,8 +1312,8 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -1325,8 +1331,8 @@ namespace Sass {
     { concrete_type(STRING); }
     static string type_name() { return "string"; }
     virtual ~String() = 0;
-    virtual bool operator==(Expression& rhs) const = 0;
-    virtual bool operator==(Expression* rhs) const = 0;
+    virtual bool operator==(const Expression& rhs) const = 0;
+    virtual bool operator==(const Expression* rhs) const = 0;
     virtual string to_string(bool compressed = false, int precision = 5) const = 0;
     ATTACH_OPERATIONS()
   };
@@ -1355,8 +1361,8 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator==(Expression& rhs) const;
-    virtual bool operator==(Expression* rhs) const;
+    virtual bool operator==(const Expression& rhs) const;
+    virtual bool operator==(const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -1395,8 +1401,8 @@ namespace Sass {
       return hash_;
     }
 
-    virtual bool operator==(Expression& rhs) const;
-    virtual bool operator==(Expression* rhs) const;
+    virtual bool operator==(const Expression& rhs) const;
+    virtual bool operator==(const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     // static char auto_quote() { return '*'; }
@@ -1416,8 +1422,8 @@ namespace Sass {
     {
       value_ = unquote(value_, &quote_mark_);
     }
-    virtual bool operator==(Expression& rhs) const;
-    virtual bool operator==(Expression* rhs) const;
+    virtual bool operator==(const Expression& rhs) const;
+    virtual bool operator==(const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
     ATTACH_OPERATIONS()
   };
@@ -1582,8 +1588,8 @@ namespace Sass {
       return -1;
     }
 
-    virtual bool operator== (Expression& rhs) const;
-    virtual bool operator== (Expression* rhs) const;
+    virtual bool operator== (const Expression& rhs) const;
+    virtual bool operator== (const Expression* rhs) const;
     virtual string to_string(bool compressed = false, int precision = 5) const;
 
     ATTACH_OPERATIONS()
@@ -1692,7 +1698,6 @@ namespace Sass {
       media_block_(0)
     { concrete_type(SELECTOR); }
     virtual ~Selector() = 0;
-    // virtual Selector_Placeholder* find_placeholder();
     virtual unsigned long specificity() {
       return Constants::Specificity_Universal;
     }
@@ -1719,13 +1724,15 @@ namespace Sass {
   class Simple_Selector : public Selector {
     ADD_PROPERTY(string, ns);
     ADD_PROPERTY(string, name)
+    ADD_PROPERTY(bool, has_ns)
   public:
     Simple_Selector(ParserState pstate, string n = "")
-    : Selector(pstate), ns_(""), name_(n)
+    : Selector(pstate), ns_(""), name_(n), has_ns_(false)
     {
       size_t pos = n.find('|');
       // found some namespace
       if (pos != string::npos) {
+        has_ns_ = true;
         ns_ = n.substr(0, pos);
         name_ = n.substr(pos + 1);
       }
@@ -1733,9 +1740,35 @@ namespace Sass {
     virtual string ns_name() const
     {
       string name("");
-      if (!ns_.empty())
+      if (has_ns_)
         name += ns_ + "|";
       return name + name_;
+    }
+    // namespace query functions
+    bool is_universal_ns() const
+    {
+      return has_ns_ && ns_ == "*";
+    }
+    bool has_universal_ns() const
+    {
+      return !has_ns_ || ns_ == "*";
+    }
+    bool is_empty_ns() const
+    {
+      return !has_ns_ || ns_ == "";
+    }
+    bool has_empty_ns() const
+    {
+      return has_ns_ && ns_ == "";
+    }
+    bool has_qualified_ns() const
+    {
+      return has_ns_ && ns_ != "" && ns_ != "*";
+    }
+    // name query functions
+    bool is_universal() const
+    {
+      return name_ == "*";
     }
 
     virtual ~Simple_Selector() = 0;
@@ -1802,6 +1835,7 @@ namespace Sass {
       if (name() == "*") return Constants::Specificity_Universal;
       else               return Constants::Specificity_Type;
     }
+    virtual Simple_Selector* unify_with(Simple_Selector*, Context&);
     virtual Compound_Selector* unify_with(Compound_Selector*, Context&);
     ATTACH_OPERATIONS()
   };
@@ -1910,6 +1944,8 @@ namespace Sass {
     {
       return selector_ ? selector_->specificity() : 0;
     }
+    bool operator==(const Simple_Selector& rhs) const;
+    bool operator==(const Wrapped_Selector& rhs) const;
     ATTACH_OPERATIONS()
   };
 
@@ -1944,6 +1980,13 @@ namespace Sass {
       }
       return false;
     };
+
+    bool is_universal() const
+    {
+      return length() == 1 && (*this)[0]->is_universal();
+    }
+
+    Complex_Selector* to_complex(Memory_Manager<AST_Node>& mem);
     Compound_Selector* unify_with(Compound_Selector* rhs, Context& ctx);
     // virtual Selector_Placeholder* find_placeholder();
     virtual bool has_parent_ref();
@@ -2020,17 +2063,33 @@ namespace Sass {
       if ((h && h->has_placeholder()) || (t && t->has_placeholder())) has_placeholder(true);
     }
     virtual bool has_parent_ref();
+
+    // can still have a tail
+    bool is_empty_ancestor() const
+    {
+      return (!head() || head()->length() == 0) &&
+             combinator() == Combinator::ANCESTOR_OF;
+    }
+
     Complex_Selector* context(Context&);
-    Complex_Selector* innermost();
-    Complex_Selector* last() { return innermost(); };
-    Complex_Selector* first() {
-      Complex_Selector* s = tail();
-      while (s && s->head() && s->head()->length() == 1 && dynamic_cast<Parent_Selector*>((*s->head())[0])) {
-        s = s->tail();
-      }
-      return s;
-    };
-    size_t length();
+
+
+    // front returns the first real tail
+    // skips over parent and empty ones
+    const Complex_Selector* first() const;
+
+    // last returns the last real tail
+    const Complex_Selector* last() const;
+
+    // unconstant accessors
+    Complex_Selector* first();
+    Complex_Selector* last();
+
+    // some shortcuts that should be removed
+    const Complex_Selector* innermost() const { return last(); };
+    Complex_Selector* innermost() { return last(); };
+
+    size_t length() const;
     Complex_Selector* parentize(Context& ctx);
     Selector_List* parentize(Selector_List* parents, Context& ctx);
     Complex_Selector* parentize(Complex_Selector* parent, Context& ctx);
@@ -2132,7 +2191,6 @@ namespace Sass {
     virtual bool is_superselector_of(Compound_Selector* sub, string wrapping = "");
     virtual bool is_superselector_of(Complex_Selector* sub, string wrapping = "");
     virtual bool is_superselector_of(Selector_List* sub, string wrapping = "");
-
     Selector_List* unify_with(Selector_List*, Context&);
     void populate_extends(Selector_List*, Context&, ExtensionSubsetMap&);
     virtual unsigned long specificity()
@@ -2148,7 +2206,8 @@ namespace Sass {
     }
     Selector_List* clone(Context&) const;      // does not clone Compound_Selector*s
     Selector_List* cloneFully(Context&) const; // clones Compound_Selector*s
-    // vector<Complex_Selector*> members() { return elements_; }
+    virtual bool operator==(const Selector& rhs) const;
+    virtual bool operator==(const Selector_List& rhs) const;
     ATTACH_OPERATIONS()
   };
 
@@ -2178,6 +2237,11 @@ namespace Sass {
       return one == two;
     }
   }
+
+  // compare function for sorting and probably other other uses
+  struct cmp_complex_selector { inline bool operator() (const Complex_Selector* l, const Complex_Selector* r) { return (*l < *r); } };
+  struct cmp_compound_selector { inline bool operator() (const Compound_Selector* l, const Compound_Selector* r) { return (*l < *r); } };
+  struct cmp_simple_selector { inline bool operator() (const Simple_Selector* l, const Simple_Selector* r) { return (*l < *r); } };
 
 }
 

Makefile
@@ -244,7 +244,7 @@ $(DESTDIR)$(PREFIX)/include: $(DESTDIR)$(PREFIX)
 	$(MKDIR) $(DESTDIR)$(PREFIX)/include
 
 $(DESTDIR)$(PREFIX)/include/%.h: include/%.h
-	$(INSTALL) -D -v -m0644 "$<" "$@"
+	$(INSTALL) -v -m0644 "$<" "$@"
 
 install-headers: $(DESTDIR)$(PREFIX)/include/sass.h \
                  $(DESTDIR)$(PREFIX)/include/sass2scss.h \
@@ -255,15 +255,15 @@ install-headers: $(DESTDIR)$(PREFIX)/include/sass.h \
 
 $(DESTDIR)$(PREFIX)/lib/%.a: lib/%.a \
                              $(DESTDIR)$(PREFIX)/lib
-	@$(INSTALL) -D -v -m0755 "$<" "$@"
+	@$(INSTALL) -v -m0755 "$<" "$@"
 
 $(DESTDIR)$(PREFIX)/lib/%.so: lib/%.so \
                              $(DESTDIR)$(PREFIX)/lib
-	@$(INSTALL) -D -v -m0755 "$<" "$@"
+	@$(INSTALL) -v -m0755 "$<" "$@"
 
 $(DESTDIR)$(PREFIX)/lib/%.dll: lib/%.dll \
                                $(DESTDIR)$(PREFIX)/lib
-	@$(INSTALL) -D -v -m0755 "$<" "$@"
+	@$(INSTALL) -v -m0755 "$<" "$@"
 
 install-static: $(DESTDIR)$(PREFIX)/lib/libsass.a
 

src/extend.cpp
@@ -628,6 +628,7 @@ namespace Sass {
 
     bool operator()(const Node& seq) const {
       // {|s| parent_superselector?(s.first, lcs.first)}
+      if (seq.collection()->size() == 0) return false;
       return parentSuperselector(seq.collection()->front(), mLcs.collection()->front(), mCtx);
     }
   };
@@ -676,7 +677,7 @@ namespace Sass {
   template<typename ChunkerType>
   static Node chunks(Node& seq1, Node& seq2, const ChunkerType& chunker) {
     Node chunk1 = Node::createCollection();
-    while (!chunker(seq1)) {
+    while (seq1.collection()->size() && !chunker(seq1)) {
       chunk1.collection()->push_back(seq1.collection()->front());
       seq1.collection()->pop_front();
     }
@@ -1236,8 +1237,8 @@ namespace Sass {
       seqLcs.collection()->pop_front();
       diff.collection()->push_back(lcsWrapper);
 
-      groupSeq1.collection()->pop_front();
-      groupSeq2.collection()->pop_front();
+      if (groupSeq1.collection()->size()) groupSeq1.collection()->pop_front();
+      if (groupSeq2.collection()->size()) groupSeq2.collection()->pop_front();
     }
 
     DEBUG_PRINTLN(SUBWEAVE, "DIFF POST LCS: " << diff)

src/node.cpp
@@ -285,20 +285,27 @@ namespace Sass {
   Node Node::naiveTrim(Node& seqses, Context& ctx) {
 
     SourcesSet sel_set;
-    Node result = Node::createCollection();
+
+    vector<Node*> res;
 
     // Add all selectors we don't already have, everything else just add it blindly
-    for (NodeDeque::iterator seqsesIter = seqses.collection()->begin(), seqsesIterEnd = seqses.collection()->end(); seqsesIter != seqsesIterEnd; ++seqsesIter) {
+    // We iterate from the back to the front, since in ruby we probably overwrite existing the items
+    for (NodeDeque::iterator seqsesIter = seqses.collection()->end() - 1, seqsesIterEnd = seqses.collection()->begin() - 1; seqsesIter != seqsesIterEnd; --seqsesIter) {
       Node& seqs1 = *seqsesIter;
       if( seqs1.isSelector() ) {
         auto found = sel_set.find( seqs1.selector() );
         if( found == sel_set.end() ) {
           sel_set.insert(seqs1.selector());
-          result.collection()->push_back(seqs1);
+          res.push_back(&seqs1);
         }
       } else {
-        result.collection()->push_back(seqs1);
+        res.push_back(&seqs1);
       }
+    }
+    Node result = Node::createCollection();
+
+    for (size_t i = res.size() - 1; i != string::npos; --i) {
+      result.collection()->push_back(*res[i]);
     }
 
     return result;

src/util.hpp
@@ -4,7 +4,10 @@
 #include <cstdio>
 #include <vector>
 #include <string>
+#include <assert.h>
 #include "ast_fwd_decl.hpp"
+
+#define SASS_ASSERT(cond, msg) assert(cond && msg)
 
 namespace Sass {
   using namespace std;

commit 160de51eb13ec7e454af3966dcd3c54ecab0f7d6

Fix operator overloading for attribute selectors
This PR fixes an `@extend` regression with multiple attribute
selectors introduced in 0e32dca5112534a74fe12348ad8ddb567100a5a3.
Fixes #1574

src/ast.cpp
@@ -205,6 +205,10 @@ namespace Sass {
 
   bool Simple_Selector::operator== (const Simple_Selector& rhs) const
   {
+    const Attribute_Selector* ll = dynamic_cast<const Attribute_Selector*>(this);
+    const Attribute_Selector* rr = dynamic_cast<const Attribute_Selector*>(&rhs);
+    if (ll && rr) return *ll == *rr;
+
     if (is_ns_eq(ns(), rhs.ns()))
     { return name() == rhs.name(); }
     return ns() == rhs.ns();
@@ -212,6 +216,10 @@ namespace Sass {
 
   bool Simple_Selector::operator< (const Simple_Selector& rhs) const
   {
+    const Attribute_Selector* ll = dynamic_cast<const Attribute_Selector*>(this);
+    const Attribute_Selector* rr = dynamic_cast<const Attribute_Selector*>(&rhs);
+    if (ll && rr) return *ll < *rr;
+
     if (is_ns_eq(ns(), rhs.ns()))
     { return name() < rhs.name(); }
     return ns() < rhs.ns();
@@ -421,6 +429,47 @@ namespace Sass {
       }
     }
     return Simple_Selector::unify_with(rhs, ctx);
+  }
+
+  bool Attribute_Selector::operator< (const Attribute_Selector& rhs) const
+  {
+    if (is_ns_eq(ns(), rhs.ns())) {
+      if (name() == rhs.name()) {
+        if (matcher() == rhs.matcher()) {
+          return value() < rhs.value();
+        } else { return matcher() < rhs.matcher(); }
+      } else { return name() < rhs.name(); }
+    }
+    else return false;
+  }
+
+  bool Attribute_Selector::operator< (const Simple_Selector& rhs) const
+  {
+    if (const Attribute_Selector* w = dynamic_cast<const Attribute_Selector*>(&rhs))
+    {
+      return *this < *w;
+    }
+    if (is_ns_eq(ns(), rhs.ns()))
+    { return name() < rhs.name(); }
+    return ns() < rhs.ns();
+  }
+
+  bool Attribute_Selector::operator== (const Attribute_Selector& rhs) const
+  {
+    if (is_ns_eq(ns(), rhs.ns()) && name() == rhs.name())
+    { return matcher() == rhs.matcher() && value() == rhs.value(); }
+    else return false;
+  }
+
+  bool Attribute_Selector::operator== (const Simple_Selector& rhs) const
+  {
+    if (const Attribute_Selector* w = dynamic_cast<const Attribute_Selector*>(&rhs))
+    {
+      return *this == *w;
+    }
+    if (is_ns_eq(ns(), rhs.ns()))
+    { return name() == rhs.name(); }
+    return ns() == rhs.ns();
   }
 
   bool Wrapped_Selector::operator== (const Wrapped_Selector& rhs) const

src/ast.hpp
@@ -1926,6 +1926,10 @@ namespace Sass {
     {
       return Constants::Specificity_Attr;
     }
+    bool operator==(const Simple_Selector& rhs) const;
+    bool operator==(const Attribute_Selector& rhs) const;
+    bool operator<(const Simple_Selector& rhs) const;
+    bool operator<(const Attribute_Selector& rhs) const;
     ATTACH_OPERATIONS()
   };
 