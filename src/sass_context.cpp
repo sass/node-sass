49b451c23dd9c31af3eb276cf9b77a50d9639977
Marcel Greter <marcel.greter@ocbnet.ch>
2016-01-09 16:13:05 +0100

    Disable error for empty source string on context

--- src/sass_context.cpp
+++ src/sass_context.cpp
@@ -427,7 +427,8 @@ extern "C" {
       return data_ctx->error_status;
     try {
       if (data_ctx->source_string == 0) { throw(std::runtime_error("Data context has no source string")); }
-      if (*data_ctx->source_string == 0) { throw(std::runtime_error("Data context has empty source string")); }
+      // empty source string is a valid case, even if not really usefull (different than with file context)
+      // if (*data_ctx->source_string == 0) { throw(std::runtime_error("Data context has empty source string")); }
     }
     catch (...) { return handle_errors(data_ctx) | 1; }
     Context* cpp_ctx = new Data_Context(data_ctx);
