557e47cee9a1a348f2fd3d6fd0d9c65f3adcc071
Marcel Greter <marcel.greter@ocbnet.ch>
2016-01-17 05:06:40 +0100

    Improve pseudo selector parsing

--- src/parser.hpp
+++ src/parser.hpp
@@ -99,6 +99,15 @@ namespace Sass {
     // peek will only skip over space, tabs and line comment
     // return the position where the lexer match will occur
     template <Prelexer::prelexer mx>
+    const char* match(const char* start = 0)
+    {
+      // match the given prelexer
+      return mx(position);
+    }
+
+    // peek will only skip over space, tabs and line comment
+    // return the position where the lexer match will occur
+    template <Prelexer::prelexer mx>
     const char* peek(const char* start = 0)
     {
 
