1a4dd3b8bcabe4567a937191ce56b258aaf6fba4
Marcel Greter <marcel.greter@ocbnet.ch>
2016-04-22 00:29:56 +0200

    Remove trailing white-space from some 3rd party files

--- src/json.cpp
+++ src/json.cpp
@@ -552,7 +552,7 @@ static void append_node(JsonNode *parent, JsonNode *child)
       child->parent = parent;
       child->prev = parent->children.tail;
       child->next = NULL;
-      
+
       if (parent->children.tail != NULL)
           parent->children.tail->next = child;
       else
@@ -567,7 +567,7 @@ static void prepend_node(JsonNode *parent, JsonNode *child)
       child->parent = parent;
       child->prev = NULL;
       child->next = parent->children.head;
-      
+
       if (parent->children.head != NULL)
           parent->children.head->prev = child;
       else
@@ -589,7 +589,7 @@ void json_append_element(JsonNode *array, JsonNode *element)
   if (array != NULL && element !=NULL) {
       assert(array->tag == JSON_ARRAY);
       assert(element->parent == NULL);
-      
+
       append_node(array, element);
   }
 }
@@ -607,7 +607,7 @@ void json_append_member(JsonNode *object, const char *key, JsonNode *value)
   if (object != NULL && key != NULL && value != NULL) {
       assert(object->tag == JSON_OBJECT);
       assert(value->parent == NULL);
-      
+
       append_member(object, json_strdup(key), value);
   }
 }
@@ -617,7 +617,7 @@ void json_prepend_member(JsonNode *object, const char *key, JsonNode *value)
   if (object != NULL && key != NULL && value != NULL) {
       assert(object->tag == JSON_OBJECT);
       assert(value->parent == NULL);
-      
+
       value->key = json_strdup(key);
       prepend_node(object, value);
   }
@@ -627,20 +627,20 @@ void json_remove_from_parent(JsonNode *node)
 {
   if (node != NULL) {
       JsonNode *parent = node->parent;
-      
+
       if (parent != NULL) {
           if (node->prev != NULL)
               node->prev->next = node->next;
           else
               parent->children.head = node->next;
-		  
+
           if (node->next != NULL)
               node->next->prev = node->prev;
           else
               parent->children.tail = node->prev;
-          
+
           free(node->key);
-          
+
           node->parent = NULL;
           node->prev = node->next = NULL;
           node->key = NULL;
--- src/json.hpp
+++ src/json.hpp
@@ -43,21 +43,21 @@ struct JsonNode
   /* only if parent is an object or array (NULL otherwise) */
   JsonNode *parent;
   JsonNode *prev, *next;
-  
+
   /* only if parent is an object (NULL otherwise) */
   char *key; /* Must be valid UTF-8. */
-  
+
   JsonTag tag;
   union {
     /* JSON_BOOL */
     bool bool_;
-    
+
     /* JSON_STRING */
     char *string_; /* Must be valid UTF-8. */
-    
+
     /* JSON_NUMBER */
     double number_;
-    
+
     /* JSON_ARRAY */
     /* JSON_OBJECT */
     struct {
