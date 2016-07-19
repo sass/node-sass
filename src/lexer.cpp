db1d56b064920daaea5e26a131ba85e6a05d4909
Marcel Greter <marcel.greter@ocbnet.ch>
2016-07-19 19:20:43 +0200

    Allow unicode chars in unquoted urls
    Fixes https://github.com/sass/libsass/issues/2120

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -88,11 +88,12 @@ namespace Sass {
     }
 
     // check if char is within a reduced ascii range
-    // valid in a uri (copied from Ruby Sass)
+    // valid in a uri (and also unicode octets)
     bool is_uri_character(const char& chr)
     {
-      return (unsigned(chr) > 41 && unsigned(chr) < 127) ||
-             unsigned(chr) == ':' || unsigned(chr) == '/';
+      return unsigned(chr) > 41 ||
+             unsigned(chr) == ':' ||
+             unsigned(chr) == '/';
     }
 
     // check if char is within a reduced ascii range
