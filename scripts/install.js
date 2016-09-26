5dba770e05ea37fb833091b8f6d560a8683db9ae
xzyfer <xzyfer@gmail.com>
2016-09-26 20:15:32 +1000

    Factor our proxy resolution for testability

--- scripts/install.js
+++ scripts/install.js
@@ -10,6 +10,7 @@ var fs = require('fs'),
   request = require('request'),
   log = require('npmlog'),
   pkg = require('../package.json'),
+  proxy = require('./util/proxy'),
   userAgent = require('./util/useragent');
 
 /**
@@ -51,7 +52,7 @@ function download(url, dest, cb) {
 
   var options = {
     rejectUnauthorized: false,
-    proxy: getProxy(),
+    proxy: proxy(),
     timeout: 60000,
     headers: {
       'User-Agent': userAgent(),
@@ -93,28 +94,6 @@ function download(url, dest, cb) {
   } catch (err) {
     cb(err);
   }
-}
-
-/**
- * Determine the proxy settings configured by npm
- *
- * It's possible to configure npm to use a proxy different
- * from the system defined proxy. This can be done via the
- * `npm config` CLI or the `.npmrc` config file.
- *
- * If a proxy has been configured in this way we must
- * tell request explicitly to use it.
- *
- * Otherwise we can trust request to the right thing.
- *
- * @return {String} the proxy configured by npm or an empty string
- * @api private
- */
-function getProxy() {
-  return '' ||
-    process.env.npm_config_https_proxy ||
-    process.env.npm_config_proxy ||
-    process.env.npm_config_http_proxy;
 }
 
 /**
