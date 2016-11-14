e7ad0cb328f8ebf45cc1ba91c56b68d8d53607c6
xzyfer <xzyfer@gmail.com>
2016-11-15 08:42:45 +1100

    Send npmlog to stdout
    Looks the default of stderr is causing issue for some CI servers.
    
    Fixes #1801

--- scripts/install.js
+++ scripts/install.js
@@ -11,6 +11,8 @@ var fs = require('fs'),
   log = require('npmlog'),
   downloadOptions = require('./util/downloadoptions');
 
+log.stream = process.stdout;
+
 /**
  * Download file, if succeeds save, if not delete
  *
@@ -110,6 +112,7 @@ function checkAndDownloadBinary() {
   try {
     mkdir.sync(path.dirname(binaryPath));
   } catch (err) {
+    log.stream = process.stderr;
     log.error('node-sass install', 'Unable to save binary to %s: %s', path.dirname(binaryPath), err);
     return;
   }
@@ -122,6 +125,7 @@ function checkAndDownloadBinary() {
 
   download(sass.getBinaryUrl(), binaryPath, function(err) {
     if (err) {
+      log.stream = process.stderr;
       log.error('node-sass install', err);
       return;
     }
