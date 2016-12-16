mgreter/libsass

Implement Reference Counted Memory Handling

src/listize.hpp
@@ -11,25 +11,25 @@
 
 namespace Sass {
 
-  typedef Environment<AST_Node*> Env;
+  typedef Environment<AST_Node_Obj> Env;
   struct Backtrace;
 
-  class Listize : public Operation_CRTP<Expression*, Listize> {
+  class Listize : public Operation_CRTP<Expression_Ptr, Listize> {
 
     Memory_Manager& mem;
 
-    Expression* fallback_impl(AST_Node* n);
+    Expression_Ptr fallback_impl(AST_Node_Ptr n);
 
   public:
     Listize(Memory_Manager&);
     ~Listize() { }
 
-    Expression* operator()(CommaSequence_Selector*);
-    Expression* operator()(Sequence_Selector*);
-    Expression* operator()(SimpleSequence_Selector*);
+    Expression_Ptr operator()(CommaComplex_Selector_Ptr);
+    Expression_Ptr operator()(Complex_Selector_Ptr);
+    Expression_Ptr operator()(Compound_Selector_Ptr);
 
     template <typename U>
-    Expression* fallback(U x) { return fallback_impl(x); }
+    Expression_Ptr fallback(U x) { return fallback_impl(x); }
   };
 
 }

src/remove_placeholders.cpp
@@ -10,15 +10,16 @@ namespace Sass {
     : ctx(ctx)
     { }
 
-    void Remove_Placeholders::operator()(Block* b) {
+    void Remove_Placeholders::operator()(Block_Ptr b) {
         for (size_t i = 0, L = b->length(); i < L; ++i) {
-            (*b)[i]->perform(this);
+            Statement_Ptr st = &b->at(i);
+            st->perform(this);
         }
     }
 
-    CommaSequence_Selector* Remove_Placeholders::remove_placeholders(CommaSequence_Selector* sl)
+    CommaComplex_Selector_Ptr Remove_Placeholders::remove_placeholders(CommaComplex_Selector_Ptr sl)
     {
-      CommaSequence_Selector* new_sl = SASS_MEMORY_NEW(ctx.mem, CommaSequence_Selector, sl->pstate());
+      CommaComplex_Selector_Ptr new_sl = SASS_MEMORY_NEW(ctx.mem, CommaComplex_Selector, sl->pstate());
 
       for (size_t i = 0, L = sl->length(); i < L; ++i) {
           if (!(*sl)[i]->contains_placeholder()) {
@@ -31,21 +32,21 @@ namespace Sass {
     }
 
 
-    void Remove_Placeholders::operator()(Ruleset* r) {
+    void Remove_Placeholders::operator()(Ruleset_Ptr r) {
         // Create a new selector group without placeholders
-        CommaSequence_Selector* sl = static_cast<CommaSequence_Selector*>(r->selector());
+        CommaComplex_Selector_Obj sl = SASS_MEMORY_CAST(CommaComplex_Selector, r->selector());
 
         if (sl) {
           // Set the new placeholder selector list
-          r->selector(remove_placeholders(sl));
+          r->selector(remove_placeholders(&sl));
           // Remove placeholders in wrapped selectors
-          for (Sequence_Selector* cs : *sl) {
+          for (Complex_Selector_Obj cs : sl->elements()) {
             while (cs) {
               if (cs->head()) {
-                for (Simple_Selector* ss : *cs->head()) {
-                  if (Wrapped_Selector* ws = dynamic_cast<Wrapped_Selector*>(ss)) {
-                    if (CommaSequence_Selector* sl = dynamic_cast<CommaSequence_Selector*>(ws->selector())) {
-                      CommaSequence_Selector* clean = remove_placeholders(sl);
+                for (Simple_Selector_Obj& ss : cs->head()->elements()) {
+                  if (Wrapped_Selector_Ptr ws = SASS_MEMORY_CAST(Wrapped_Selector, ss)) {
+                    if (CommaComplex_Selector_Ptr sl = SASS_MEMORY_CAST(CommaComplex_Selector, ws->selector())) {
+                      CommaComplex_Selector_Ptr clean = remove_placeholders(sl);
                       // also clean superflous parent selectors
                       // probably not really the correct place
                       clean->remove_parent_selectors();
@@ -60,22 +61,25 @@ namespace Sass {
         }
 
         // Iterate into child blocks
-        Block* b = r->block();
+        Block_Obj b = r->oblock();
 
         for (size_t i = 0, L = b->length(); i < L; ++i) {
-            if ((*b)[i]) (*b)[i]->perform(this);
+            if (b->at(i)) {
+                Statement_Obj st = b->at(i);
+                st->perform(this);
+            }
         }
     }
 
-    void Remove_Placeholders::operator()(Media_Block* m) {
-        operator()(m->block());
+    void Remove_Placeholders::operator()(Media_Block_Ptr m) {
+        operator()(&m->oblock());
     }
-    void Remove_Placeholders::operator()(Supports_Block* m) {
-        operator()(m->block());
+    void Remove_Placeholders::operator()(Supports_Block_Ptr m) {
+        operator()(&m->oblock());
     }
 
-    void Remove_Placeholders::operator()(Directive* a) {
-        if (a->block()) a->block()->perform(this);
+    void Remove_Placeholders::operator()(Directive_Ptr a) {
+        if (a->oblock()) a->oblock()->perform(this);
     }
 
 }

src/to_value.hpp
@@ -7,9 +7,9 @@
 
 namespace Sass {
 
-  class To_Value : public Operation_CRTP<Value*, To_Value> {
+  class To_Value : public Operation_CRTP<Value_Ptr, To_Value> {
 
-    Value* fallback_impl(AST_Node* n);
+    Value_Ptr fallback_impl(AST_Node_Ptr n);
 
   private:
 
@@ -22,27 +22,27 @@ namespace Sass {
     : ctx(ctx), mem(mem)
     { }
     ~To_Value() { }
-    using Operation<Value*>::operator();
+    using Operation<Value_Ptr>::operator();
 
-    Value* operator()(Argument*);
-    Value* operator()(Boolean*);
-    Value* operator()(Number*);
-    Value* operator()(Color*);
-    Value* operator()(String_Constant*);
-    Value* operator()(String_Quoted*);
-    Value* operator()(Custom_Warning*);
-    Value* operator()(Custom_Error*);
-    Value* operator()(List*);
-    Value* operator()(Map*);
-    Value* operator()(Null*);
+    Value_Ptr operator()(Argument_Ptr);
+    Value_Ptr operator()(Boolean_Ptr);
+    Value_Ptr operator()(Number_Ptr);
+    Value_Ptr operator()(Color_Ptr);
+    Value_Ptr operator()(String_Constant_Ptr);
+    Value_Ptr operator()(String_Quoted_Ptr);
+    Value_Ptr operator()(Custom_Warning_Ptr);
+    Value_Ptr operator()(Custom_Error_Ptr);
+    Value_Ptr operator()(List_Ptr);
+    Value_Ptr operator()(Map_Ptr);
+    Value_Ptr operator()(Null_Ptr);
 
     // convert to string via `To_String`
-    Value* operator()(CommaSequence_Selector*);
-    Value* operator()(Binary_Expression*);
+    Value_Ptr operator()(CommaComplex_Selector_Ptr);
+    Value_Ptr operator()(Binary_Expression_Ptr);
 
     // fallback throws error
     template <typename U>
-    Value* fallback(U x) { return fallback_impl(x); }
+    Value_Ptr fallback(U x) { return fallback_impl(x); }
   };
 
 }

sass/node-sass

src/listize.hpp
@@ -11,25 +11,23 @@
 
 namespace Sass {
 
-  typedef Environment<AST_Node*> Env;
+  typedef Environment<AST_Node_Obj> Env;
   struct Backtrace;
 
-  class Listize : public Operation_CRTP<Expression*, Listize> {
+  class Listize : public Operation_CRTP<Expression_Ptr, Listize> {
 
-    Memory_Manager& mem;
-
-    Expression* fallback_impl(AST_Node* n);
+    Expression_Ptr fallback_impl(AST_Node_Ptr n);
 
   public:
-    Listize(Memory_Manager&);
+    Listize();
     ~Listize() { }
 
-    Expression* operator()(CommaSequence_Selector*);
-    Expression* operator()(Sequence_Selector*);
-    Expression* operator()(SimpleSequence_Selector*);
+    Expression_Ptr operator()(Selector_List_Ptr);
+    Expression_Ptr operator()(Complex_Selector_Ptr);
+    Expression_Ptr operator()(Compound_Selector_Ptr);
 
     template <typename U>
-    Expression* fallback(U x) { return fallback_impl(x); }
+    Expression_Ptr fallback(U x) { return fallback_impl(x); }
   };
 
 }

src/remove_placeholders.cpp
@@ -10,19 +10,20 @@ namespace Sass {
     : ctx(ctx)
     { }
 
-    void Remove_Placeholders::operator()(Block* b) {
+    void Remove_Placeholders::operator()(Block_Ptr b) {
         for (size_t i = 0, L = b->length(); i < L; ++i) {
-            (*b)[i]->perform(this);
+            Statement_Ptr st = &b->at(i);
+            st->perform(this);
         }
     }
 
-    CommaSequence_Selector* Remove_Placeholders::remove_placeholders(CommaSequence_Selector* sl)
+    Selector_List_Ptr Remove_Placeholders::remove_placeholders(Selector_List_Ptr sl)
     {
-      CommaSequence_Selector* new_sl = SASS_MEMORY_NEW(ctx.mem, CommaSequence_Selector, sl->pstate());
+      Selector_List_Ptr new_sl = SASS_MEMORY_NEW(Selector_List, sl->pstate());
 
       for (size_t i = 0, L = sl->length(); i < L; ++i) {
-          if (!(*sl)[i]->contains_placeholder()) {
-              *new_sl << (*sl)[i];
+          if (!sl->at(i)->contains_placeholder()) {
+              new_sl->append(sl->at(i));
           }
       }
 
@@ -31,21 +32,21 @@ namespace Sass {
     }
 
 
-    void Remove_Placeholders::operator()(Ruleset* r) {
+    void Remove_Placeholders::operator()(Ruleset_Ptr r) {
         // Create a new selector group without placeholders
-        CommaSequence_Selector* sl = static_cast<CommaSequence_Selector*>(r->selector());
+        Selector_List_Obj sl = SASS_MEMORY_CAST(Selector_List, r->selector());
 
         if (sl) {
           // Set the new placeholder selector list
-          r->selector(remove_placeholders(sl));
+          r->selector(remove_placeholders(&sl));
           // Remove placeholders in wrapped selectors
-          for (Sequence_Selector* cs : *sl) {
+          for (Complex_Selector_Obj cs : sl->elements()) {
             while (cs) {
               if (cs->head()) {
-                for (Simple_Selector* ss : *cs->head()) {
-                  if (Wrapped_Selector* ws = dynamic_cast<Wrapped_Selector*>(ss)) {
-                    if (CommaSequence_Selector* sl = dynamic_cast<CommaSequence_Selector*>(ws->selector())) {
-                      CommaSequence_Selector* clean = remove_placeholders(sl);
+                for (Simple_Selector_Obj& ss : cs->head()->elements()) {
+                  if (Wrapped_Selector_Ptr ws = SASS_MEMORY_CAST(Wrapped_Selector, ss)) {
+                    if (Selector_List_Ptr sl = SASS_MEMORY_CAST(Selector_List, ws->selector())) {
+                      Selector_List_Ptr clean = remove_placeholders(sl);
                       // also clean superflous parent selectors
                       // probably not really the correct place
                       clean->remove_parent_selectors();
@@ -60,21 +61,24 @@ namespace Sass {
         }
 
         // Iterate into child blocks
-        Block* b = r->block();
+        Block_Obj b = r->block();
 
         for (size_t i = 0, L = b->length(); i < L; ++i) {
-            if ((*b)[i]) (*b)[i]->perform(this);
+            if (b->at(i)) {
+                Statement_Obj st = b->at(i);
+                st->perform(this);
+            }
         }
     }
 
-    void Remove_Placeholders::operator()(Media_Block* m) {
-        operator()(m->block());
+    void Remove_Placeholders::operator()(Media_Block_Ptr m) {
+        operator()(&m->block());
     }
-    void Remove_Placeholders::operator()(Supports_Block* m) {
-        operator()(m->block());
+    void Remove_Placeholders::operator()(Supports_Block_Ptr m) {
+        operator()(&m->block());
     }
 
-    void Remove_Placeholders::operator()(Directive* a) {
+    void Remove_Placeholders::operator()(Directive_Ptr a) {
         if (a->block()) a->block()->perform(this);
     }
 

src/to_value.hpp
@@ -7,42 +7,41 @@
 
 namespace Sass {
 
-  class To_Value : public Operation_CRTP<Value*, To_Value> {
+  class To_Value : public Operation_CRTP<Value_Ptr, To_Value> {
 
-    Value* fallback_impl(AST_Node* n);
+    Value_Ptr fallback_impl(AST_Node_Ptr n);
 
   private:
 
     Context& ctx;
-    Memory_Manager& mem;
 
   public:
 
-    To_Value(Context& ctx, Memory_Manager& mem)
-    : ctx(ctx), mem(mem)
+    To_Value(Context& ctx)
+    : ctx(ctx)
     { }
     ~To_Value() { }
-    using Operation<Value*>::operator();
+    using Operation<Value_Ptr>::operator();
 
-    Value* operator()(Argument*);
-    Value* operator()(Boolean*);
-    Value* operator()(Number*);
-    Value* operator()(Color*);
-    Value* operator()(String_Constant*);
-    Value* operator()(String_Quoted*);
-    Value* operator()(Custom_Warning*);
-    Value* operator()(Custom_Error*);
-    Value* operator()(List*);
-    Value* operator()(Map*);
-    Value* operator()(Null*);
+    Value_Ptr operator()(Argument_Ptr);
+    Value_Ptr operator()(Boolean_Ptr);
+    Value_Ptr operator()(Number_Ptr);
+    Value_Ptr operator()(Color_Ptr);
+    Value_Ptr operator()(String_Constant_Ptr);
+    Value_Ptr operator()(String_Quoted_Ptr);
+    Value_Ptr operator()(Custom_Warning_Ptr);
+    Value_Ptr operator()(Custom_Error_Ptr);
+    Value_Ptr operator()(List_Ptr);
+    Value_Ptr operator()(Map_Ptr);
+    Value_Ptr operator()(Null_Ptr);
 
     // convert to string via `To_String`
-    Value* operator()(CommaSequence_Selector*);
-    Value* operator()(Binary_Expression*);
+    Value_Ptr operator()(Selector_List_Ptr);
+    Value_Ptr operator()(Binary_Expression_Ptr);
 
     // fallback throws error
     template <typename U>
-    Value* fallback(U x) { return fallback_impl(x); }
+    Value_Ptr fallback(U x) { return fallback_impl(x); }
   };
 
 }