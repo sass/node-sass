5f5e31e5d79f129772e5d6dc26b7de055269a2bc
Marcel Greter <marcel.greter@ocbnet.ch>
2018-01-20 22:17:33 +0100

    Fix msvc compiler warning

--- src/parser.cpp
+++ src/parser.cpp
@@ -2654,6 +2654,7 @@ namespace Sass {
         re_selector_list
       >(p)
     ) {
+      bool could_be_property = peek< sequence< exactly<'-'>, exactly<'-'> > >(p) != 0;
       while (p < q) {
         // did we have interpolations?
         if (*p == '#' && *(p+1) == '{') {
