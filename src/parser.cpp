60f839158eff1f1bb84cb44f0f0c4bce3ed86a6d
xzyfer <xzyfer@gmail.com>
2018-03-06 00:40:40 +1100

    Fix parsing of @supports declarations
    Emulate the parse behaviour of `@media` query declarations.
    
    Fixes #2452

--- src/parser.cpp
+++ src/parser.cpp
@@ -2448,12 +2448,16 @@ namespace Sass {
   {
     Supports_Condition_Ptr cond;
     // parse something declaration like
-    Declaration_Obj declaration = parse_declaration();
-    if (!declaration) error("@supports condition expected declaration", pstate);
+    Expression_Obj feature = parse_expression();
+    Expression_Obj expression = 0;
+    if (lex_css< exactly<':'> >()) {
+      expression = parse_list(DELAYED);
+    }
+    if (!feature || !expression) error("@supports condition expected declaration", pstate);
     cond = SASS_MEMORY_NEW(Supports_Declaration,
-                     declaration->pstate(),
-                     declaration->property(),
-                     declaration->value());
+                     feature->pstate(),
+                     feature,
+                     expression);
     // ToDo: maybe we need an additional error condition?
     return cond;
   }
