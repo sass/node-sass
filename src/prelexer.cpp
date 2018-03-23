c0a5a21664edb24a03ac1c94927ccb167dfa8f0e
xzyfer <xzyfer@gmail.com>
2018-03-23 22:43:01 +1100

    Allow "!" in custom property values
    Fixes #2590
    
    Spec https://github.com/sass/sass-spec/pull/1240

--- src/prelexer.cpp
+++ src/prelexer.cpp
@@ -1606,7 +1606,7 @@ namespace Sass {
       >(src);
     }
 
-    extern const char css_variable_url_top_level_negates[] = "()[]{}\"'#/;!";
+    extern const char css_variable_url_top_level_negates[] = "()[]{}\"'#/;";
     const char* css_variable_top_level_value(const char* src) {
       return sequence<
         alternatives<
